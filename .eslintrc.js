78dccc2ca27fec034aba5b23461fe064798afd81
Cheton Wu <cheton@gmail.com>
2020-01-18 11:03:59 +0800

    chore: project initialization

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  extends: 'trendmicro',
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+  },
+  plugins: [
+    'react-hooks',
+  ],
+  rules: {
+    'indent': ['error', 2],
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+  },
+};
---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: '@trendmicro/babel-config',
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@emotion/babel-preset-css-prop',
+  ],
+};
