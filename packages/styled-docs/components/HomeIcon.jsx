e4e3e937aeaca372962d1dbdb3dede17e5a02902
Cheton Wu <cheton@gmail.com>
2020-02-04 15:28:51 +0800

    docs: refine SVGIcon docs

--- packages/styled-docs/components/HomeIcon.jsx
+++ packages/styled-docs/components/HomeIcon.jsx
@@ -3,10 +3,12 @@ import { SVGIcon } from '@trendmicro/react-styled-core';
 const HomeIcon = (props) => (
   <SVGIcon
     fontSize="1.5rem"
-    mx="1rem"
+    mx=".75rem"
     {...props}
   >
-    <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    <g fill="currentColor">
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </g>
   </SVGIcon>
 );
 
