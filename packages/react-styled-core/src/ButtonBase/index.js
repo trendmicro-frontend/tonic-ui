babbdbc79dd1ba197abc898e7b0f78245b18afb1
Cheton Wu <chetonu@gmail.com>
2020-02-12 10:49:14 +0800

    chore(ButtonBase): refine base style props

--- packages/react-styled-core/src/ButtonBase/index.js
+++ packages/react-styled-core/src/ButtonBase/index.js
@@ -1,40 +1,39 @@
 import React, { forwardRef } from 'react';
 import PseudoBox from '../PseudoBox';
 
-const ButtonBase = forwardRef(
-  (
-    {
-      disabled,
-      children,
-      as: Comp = 'button',
-      type = 'button',
-      ...rest
-    },
-    ref,
-  ) => {
-    // `ButtonBase` has no any styles. (no border, no padding, no shadow, no background)
-    const removedStyles = {
-      bg: 'transparent',
-      border: 'none',
-      outline: 'none',
-      padding: 0,
-    };
-
-    return (
-      <PseudoBox
-        disabled={disabled}
-        aria-disabled={disabled}
-        ref={ref}
-        as={Comp}
-        type={type}
-        {...removedStyles}
-        {...rest}
-      >
-        { children }
-      </PseudoBox>
-    );
+const ButtonBase = forwardRef((
+  {
+    disabled,
+    children,
+    as: Comp = 'button',
+    type = 'button',
+    ...rest
   },
-);
+  ref,
+) => {
+  // The base button does not have appearance including default color, padding, outline, and border settings
+  const baseStyleProps = {
+    bg: 'transparent',
+    border: 'none',
+    outline: 0,
+    padding: 0,
+    cursor: 'pointer',
+  };
+
+  return (
+    <PseudoBox
+      disabled={disabled}
+      aria-disabled={disabled}
+      ref={ref}
+      as={Comp}
+      type={type}
+      {...baseStyleProps}
+      {...rest}
+    >
+      {children}
+    </PseudoBox>
+  );
+});
 
 ButtonBase.displayName = 'ButtonBase';
 
