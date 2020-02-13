58b8a373bea66be3e78d87e2829257e1b7b5d69f
Tina C Lin (RD-TW) <tina_c_lin@trendmicro.com>
2020-02-13 16:47:38 +0800

    fix: all buttons should use the same variant and size in a group.

--- packages/react-styled-core/src/ButtonGroup/index.js
+++ packages/react-styled-core/src/ButtonGroup/index.js
@@ -4,13 +4,11 @@ import Box from '../Box';
 const ButtonGroup = ({
   children,
   size = 'md',
-  variantColor,
   variant = 'solid',
+  variantColor,
   vertical,
   ...rest
 }) => {
-  let prevChildVariant;
-
   const clones = Children.map(children, (child, index) => {
     if (!isValidElement(child)) {
       return false;
@@ -18,23 +16,22 @@ const ButtonGroup = ({
 
     const isFirst = index === 0;
     const isLast = index === Children.count(children) - 1;
-    const _variant = child.props.variant || variant;
     const horizontalProps = {
       ...(isFirst && { borderTopRightRadius: 0, borderBottomRightRadius: 0 }),
       ...(isLast && { borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }),
-      ...(!isFirst && prevChildVariant === 'outline' && _variant === 'outline' && { ml: -1 }),
+      ...(!isFirst && variant === 'outline' && { ml: -1 }),
       ...(!isFirst && !isLast && { borderRadius: 0 }),
     };
     const verticalProps = {
       ...(isFirst && { borderBottomLeftRadius: 0, borderBottomRightRadius: 0 }),
       ...(isLast && { borderTopLeftRadius: 0, borderTopRightRadius: 0 }),
-      ...(!isFirst && prevChildVariant === 'outline' && _variant === 'outline' && { mt: -1 }),
+      ...(!isFirst && variant === 'outline' && { mt: -1 }),
       ...(!isFirst && !isLast && { borderRadius: 0 }),
     };
     const buttonStyleProps = vertical ? verticalProps : horizontalProps;
 
     let hr = null;
-    if (!isFirst && prevChildVariant === 'solid' && _variant === 'solid') {
+    if (!isFirst && variant === 'solid') {
       hr = vertical ? (
         <Box height="1px" bg="gray.70" />
       ) : (
@@ -42,16 +39,14 @@ const ButtonGroup = ({
       );
     }
 
-    prevChildVariant = _variant;
-
     return (
       <Fragment>
         { hr }
         {
           cloneElement(child, {
-            size: child.props.size || size,
+            size: size,
             variantColor: child.props.variantColor || variantColor,
-            variant: _variant,
+            variant: variant,
             ...buttonStyleProps
           })
         }
