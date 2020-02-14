1aa802cd159b3376d5d62305d05b167927d3c163
Cheton Wu <chetonu@gmail.com>
2020-02-14 15:30:46 +0800

    feat: Heading component

--- packages/react-styled-core/src/Heading/index.js
+++ packages/react-styled-core/src/Heading/index.js
@@ -1,13 +1,71 @@
 import React, { forwardRef } from 'react';
 import Box from '../Box';
 
-const Heading = forwardRef((props, ref) => (
-  <Box
-    ref={ref}
-    fontFamily="heading"
-    {...props}
-  />
-));
+const defaultVariantMapping = {
+  h1: {
+    as: 'h1',
+    fontSize: '4xl',
+    lineHeight: '4xl',
+    fontWeight: 'semibold',
+    m: 0,
+  },
+  h2: {
+    as: 'h2',
+    fontSize: '2xl',
+    lineHeight: '2xl',
+    fontWeight: 'semibold',
+    margin: 0,
+  },
+  h3: {
+    as: 'h3',
+    fontSize: 'xl',
+    lineHeight: 'xl',
+    fontWeight: 'semibold',
+    margin: 0,
+  },
+  h4: {
+    as: 'h4',
+    fontSize: 'md',
+    lineHeight: 'md',
+    fontWeight: 'semibold',
+    margin: 0,
+  },
+  h5: {
+    as: 'h5',
+    fontSize: 'sm',
+    lineHeight: 'sm',
+    fontWeight: 'semibold',
+    margin: 0,
+  },
+  h6: {
+    as: 'h6',
+    fontSize: 'sm',
+    lineHeight: 'sm',
+    fontWeight: 'normal',
+    margin: 0,
+  },
+};
+
+const Heading = forwardRef((
+  {
+    variant,
+    ...rest
+  },
+  ref
+) => {
+  const variantProps = defaultVariantMapping[variant];
+
+  return (
+    <Box
+      ref={ref}
+      as={defaultVariantMapping[variant]}
+      display="block"
+      fontFamily="heading"
+      {...variantProps}
+      {...rest}
+    />
+  );
+});
 
 Heading.displayName = 'Heading';
 
