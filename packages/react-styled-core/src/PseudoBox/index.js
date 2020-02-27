b3ea43757d3a1c6ad0ac6123a16b06cb6015e54d
WU, FENG-CHIN <nikeorz+github@gmail.com>
2020-02-27 20:45:51 +0800

    Comment - Add helpful comments
    Add helpful comments for anchor pseudo classes

--- packages/react-styled-core/src/PseudoBox/index.js
+++ packages/react-styled-core/src/PseudoBox/index.js
@@ -96,6 +96,9 @@ const PseudoBox = styled(Box)(
 
     return css({
       // pseudo-classes
+      // XXX:
+      //    a:hover MUST come after a:link and a:visited in the CSS definition in order to be effective.
+      //    a:active MUST come after a:hover in the CSS definition in order to be effective.
       [focus]: _focus,
       [checked]: _checked,
       [visited]: _visited,
