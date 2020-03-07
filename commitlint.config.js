787887c797b1093145f18f1f4a0c5639137820df
Cheton Wu <cheton@gmail.com>
2020-03-07 23:38:57 +0800

    chore(commitlint): increase header-max-length to 128

--- commitlint.config.js
+++ commitlint.config.js
@@ -14,7 +14,7 @@ module.exports = {
     '@commitlint/config-conventional',
   ],
   rules: {
-    'header-max-length': [2, 'always', 96],
+    'header-max-length': [2, 'always', 128],
     'scope-case': [0],
     'subject-case': [0],
   },
