f2503ab4f9f0cb338e3c9c34e7a79feb5a57095a
Tina C Lin (RD-TW) <tina_c_lin@trendmicro.com>
2020-03-11 15:19:21 +0800

    refactor: wrap children with a context provider in CheckboxGroup component

--- packages/react-styled-core/src/CheckboxGroup/index.js
+++ packages/react-styled-core/src/CheckboxGroup/index.js
@@ -1,32 +1,18 @@
-import React, {
-  Children,
-  cloneElement,
-  useState,
-  useRef,
-  isValidElement,
-} from 'react';
-import { useId } from '../utils/autoId';
-import Box from '../Box';
+import React, { useState, useRef } from 'react';
+import GroupContext from '../GroupContext';
 
 const CheckboxGroup = ({
-  onChange,
-  name,
-  value: valueProp,
-  defaultValue,
-
-  size,
-  spacing,
-  variantColor,
-  disabled,
-  inline,
-
   children,
-  ...rest
+  defaultValue,
+  disabled,
+  size,
+  value: valueProp,
+  variantColor,
+  onChange,
 }) => {
-  const [values, setValues] = useState(defaultValue || []);
   const { current: isControlled } = useRef(valueProp != null);
+  const [values, setValues] = useState(defaultValue || []);
   const _values = isControlled ? valueProp : values;
-
   const _onChange = event => {
     const { checked, value } = event.target;
     let newValues;
@@ -35,45 +21,21 @@ const CheckboxGroup = ({
     } else {
       newValues = _values.filter(val => val !== value);
     }
-
     !isControlled && setValues(newValues);
     onChange && onChange(newValues);
   };
-
-  // If no name is passed, we'll generate a random, unique name
-  const fallbackName = `checkbox-${useId()}`;
-  const _name = name || fallbackName;
-
-  const clones = Children.map(children, (child, index) => {
-    if (!isValidElement(child)) {
-      return null;
-    }
-
-    const isLastCheckbox = (children.length === index + 1);
-    const defaultSpacing = spacing || (inline ? '6x' : '1x');
-    const spacingProps = inline ? { mr: defaultSpacing } : { mb: defaultSpacing };
-
-    return (
-      <Box
-        display={inline ? 'inline-block' : 'block'}
-        {...(!isLastCheckbox && spacingProps)}
-      >
-        {cloneElement(child, {
-          size: size,
-          variantColor: variantColor,
-          name: `${_name}-${index}`,
-          onChange: _onChange,
-          checked: _values.includes(child.props.value),
-          disabled: disabled,
-        })}
-      </Box>
-    );
-  });
+  const state = {
+    disabled: disabled,
+    onChange: _onChange,
+    size: size,
+    value: _values,
+    variantColor: variantColor,
+  };
 
   return (
-    <Box role="group" {...rest}>
-      {clones}
-    </Box>
+    <GroupContext.Provider value={state}>
+      { children }
+    </GroupContext.Provider>
   );
 };
 
