92bd56492d2f1b6f842346cd3276ce2e0d0b008a
Cheton Wu <cheton@gmail.com>
2020-03-26 23:41:18 +0800

    refactor: consistent base style

--- packages/react-styled-core/src/ButtonBase/index.js
+++ packages/react-styled-core/src/ButtonBase/index.js
@@ -1,34 +1,28 @@
 import React, { forwardRef } from 'react';
 import PseudoBox from '../PseudoBox';
+import { baseProps } from './styles';
 
+/**
+ * `ButtonBase` does not have appearance settings including default color, padding, outline, and border
+ */
 const ButtonBase = forwardRef((
   {
-    disabled,
     children,
-    as = 'button',
-    type = 'button',
     ...rest
   },
   ref,
 ) => {
-  // ButtonBase does not have appearance settings including default color, padding, outline, and border
-  const baseStyleProps = {
-    bg: 'transparent',
-    border: 'none',
-    color: 'inherit',
-    cursor: disabled ? 'default' : 'pointer',
-    outline: 0,
-    padding: 0,
-  };
+  const { disabled } = rest;
 
   return (
     <PseudoBox
+      ref={ref}
+      as="button"
+      type="button"
+      cursor={!!disabled ? 'default' : 'pointer'}
       disabled={disabled}
       aria-disabled={disabled}
-      ref={ref}
-      as={as}
-      type={type}
-      {...baseStyleProps}
+      {...baseProps}
       {...rest}
     >
       {children}
