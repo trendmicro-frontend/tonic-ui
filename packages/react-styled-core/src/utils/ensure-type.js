17282a80c7a444d797b7bd5ad71173df685a490a
Tina C Lin (RD-TW) <tina_c_lin@trendmicro.com>
2020-03-31 14:07:50 +0800

    refactor: remove default value of context from CheckboxGroup

--- packages/react-styled-core/src/utils/ensure-type.js
+++ packages/react-styled-core/src/utils/ensure-type.js
@@ -1,3 +1,13 @@
+export const ensureArray = (...args) => {
+  if (args.length === 0 || args[0] === undefined || args[0] === null) {
+    return [];
+  }
+  if (args.length === 1) {
+    return [].concat(args[0]);
+  }
+  return [].concat(args);
+};
+
 export const ensureBoolean = (value, defaultValue = false) => {
   if (value === undefined || value === null) {
     return Boolean(defaultValue);
