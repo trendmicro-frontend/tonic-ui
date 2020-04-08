f9e6fd6bffff1e81140bf9efba842b2a38262d83
Cheton Wu <cheton@gmail.com>
2020-04-08 12:20:53 +0800

    fix: eslint errors

--- packages/react-styled-core/src/shared/styled-system/utils.test.js
+++ packages/react-styled-core/src/shared/styled-system/utils.test.js
@@ -4,7 +4,7 @@ describe('getWidth', () => {
   test('no scale', () => {
     expect(getWidth('1x')).toEqual('1x');
     expect(getWidth(0)).toEqual('0%');
-    expect(getWidth(.5)).toEqual('50%');
+    expect(getWidth(0.5)).toEqual('50%');
     expect(getWidth(1)).toEqual('100%');
     expect(getWidth(2)).toEqual(2);
   });
@@ -22,7 +22,7 @@ describe('getWidth', () => {
     expect(getWidth('1rem', scale)).toEqual('1rem');
     expect(getWidth('1x', scale)).toEqual('.25rem');
     expect(getWidth(0, scale)).toEqual('0');
-    expect(getWidth(.5, scale)).toEqual('50%');
+    expect(getWidth(0.5, scale)).toEqual('50%');
     expect(getWidth(1, scale)).toEqual('100%');
     expect(getWidth(2, scale)).toEqual(2);
   });
