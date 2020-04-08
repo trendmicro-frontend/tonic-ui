0dfdcd116f3e92352553d5ff5f3f6cbb6ee0ffae
Cheton Wu <cheton@gmail.com>
2020-04-08 12:19:53 +0800

    refactor: enhance UT and update build scripts

--- packages/react-styled-core/src/shared/styled-system/layout.js
+++ packages/react-styled-core/src/shared/styled-system/layout.js
@@ -1,21 +1,5 @@
-import { get, system } from '@styled-system/core';
-
-const isFiniteNumber = n => (typeof n === 'number') && !Number.isNaN(n);
-
-/**
- * The `width` prop is transformed based on the following:
- * • Numbers from 0-1 are converted to percentage widths.
- * • Numbers greater than 1 are converted to pixel values.
- * • String values are passed as raw CSS values.
- * • Arrays are converted to responsive width styles.
- * • If `theme.sizes` is defined, the `width` prop will attempt to pick up values from the theme.
- */
-const getWidth = (n, scale) => {
-  const defaultValue = (!isFiniteNumber(n) || n > 1)
-    ? n
-    : n * 100 + '%';
-  return get(scale, n, defaultValue);
-};
+import { system } from '@styled-system/core';
+import { getWidth } from './utils';
 
 /**
  * The layout utility includes style props for the following:
