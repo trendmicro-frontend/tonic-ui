c93d7b3fbd5aeab55e9fe35c08263b26977b83d3
“Austin <austinhung81@gmail.com>
2020-04-15 11:37:00 +0800

    fix: refactor text decoration

--- packages/react-styled-core/src/Link/index.js
+++ packages/react-styled-core/src/Link/index.js
@@ -3,17 +3,18 @@ import PropTypes from 'prop-types';
 import PseudoBox from '../PseudoBox';
 import useColorMode from '../useColorMode';
 
-const baseStyleProps = (colorMode, hideUnderline) => {
+const baseStyleProps = (colorMode, disabled, textDecoration) => {
   const color = { light: 'blue:60', dark: 'blue:40' }[colorMode];
   const hoverColor = { light: 'blue:50', dark: 'blue:40' }[colorMode];
   const visitedColor = { light: 'purple:60', dark: 'purple:50' }[colorMode];
   const disabledColor = { light: 'black:disabled', dark: 'white:disabled' }[colorMode];
-
+  const hoverTextDecoration = !!disabled ? 'none' : 'underline';
+  const activeTextDecoration = !!disabled ? 'none' : 'underline';
   return {
     color,
     cursor: 'pointer',
     outline: 'none',
-    textDecoration: 'none',
+    textDecoration: textDecoration ?? 'none',
     _disabled: {
       color: disabledColor,
       cursor: 'not-allowed',
@@ -23,25 +24,25 @@ const baseStyleProps = (colorMode, hideUnderline) => {
     },
     _hover: {
       color: hoverColor,
-      textDecoration: hideUnderline ? 'none' : 'underline',
+      textDecoration: textDecoration ?? hoverTextDecoration,
     },
     _active: {
       color: 'blue:60',
-      textDecoration: hideUnderline ? 'none' : 'underline',
+      textDecoration: textDecoration ?? activeTextDecoration,
     },
   };
 };
 
-const Link = forwardRef(({ disabled, onClick, ...props }, ref) => {
+const Link = forwardRef(({ disabled, onClick, textDecoration, ...props }, ref) => {
   const { colorMode } = useColorMode();
-  const hideUnderline = props.textDecoration !== 'underline' && disabled;
+  //const hideUnderline = props.textDecoration !== 'underline' && disabled;
   return (
     <PseudoBox
       as="a"
       ref={ref}
       aria-disabled={disabled}
       onClick={disabled ? event => event.preventDefault() : onClick}
-      {...baseStyleProps(colorMode, hideUnderline)}
+      {...baseStyleProps(colorMode, disabled, textDecoration)}
       {...props}
     />
   );
