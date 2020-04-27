231316b5a1ea19b5fc12ebc9ed147cce8aee5919
“Austin <austinhung81@gmail.com>
2020-04-27 15:38:32 +0800

    fix: split Popover components and fixed issue

---
+++ packages/react-styled-ui/src/Popover/PopoverContent.js
@@ -0,0 +1,112 @@
+import { usePopover } from './context';
+import Popper, { PopperArrow } from '../Popper';
+import { usePopoverContentStyle } from './styles';
+
+const wrapEvent = (theirHandler, ourHandler) => event => {
+  theirHandler && theirHandler(event);
+  if (!event.defaultPrevented) {
+    return ourHandler(event);
+  }
+  return '';
+};
+
+const PopoverContent = ({
+  onKeyDown,
+  onBlur: onBlurProp,
+  onMouseLeave,
+  onMouseEnter,
+  onFocus,
+  children,
+  'aria-label': ariaLabel,
+  ...props
+}) => {
+  const {
+    popoverRef,
+    referenceRef,
+    placement,
+    popoverId,
+    isOpen,
+    onBlur,
+    closeOnEsc,
+    onClose,
+    isHoveringRef,
+    trigger,
+    headerId,
+    bodyId,
+    usePortal,
+    hideArrow,
+    skidding,
+    distance,
+    hideDelay,
+  } = usePopover();
+  const contentStyleProps = usePopoverContentStyle();
+  const arrowSize = 12;
+  const _distance = distance + 8; // Arrow height is 8px
+  let eventHandlers = {};
+  let roleProps = {};
+
+  if (trigger === 'click') {
+    eventHandlers = {
+      onBlur: wrapEvent(onBlurProp, onBlur),
+    };
+
+    roleProps = {
+      role: 'dialog',
+      'aria-modal': 'false',
+    };
+  }
+
+  if (trigger === 'hover') {
+    eventHandlers = {
+      onMouseEnter: wrapEvent(onMouseEnter, () => {
+        isHoveringRef.current = true;
+      }),
+      onMouseLeave: wrapEvent(onMouseLeave, () => {
+        isHoveringRef.current = false;
+        setTimeout(onClose, hideDelay);
+      }),
+    };
+
+    roleProps = {
+      role: 'tooltip',
+    };
+  }
+
+  eventHandlers = {
+    ...eventHandlers,
+    onKeyDown: wrapEvent(onKeyDown, event => {
+      if (event.key === 'Escape' && closeOnEsc) {
+        onClose && onClose();
+      }
+    }),
+  };
+
+  return (
+    <Popper
+      as="section"
+      usePortal={usePortal}
+      isOpen={isOpen}
+      placement={placement}
+      aria-label={ariaLabel}
+      anchorEl={referenceRef.current}
+      ref={popoverRef}
+      id={popoverId}
+      aria-hidden={!isOpen}
+      arrowSize={`${arrowSize}px`}
+      modifiers={{ offset: [skidding, _distance] }}
+      aria-labelledby={headerId}
+      aria-describedby={bodyId}
+      {...contentStyleProps}
+      {...roleProps}
+      {...eventHandlers}
+      {...props}
+    >
+      {!hideArrow && <PopperArrow />}
+      {children}
+    </Popper>
+  );
+};
+
+PopoverContent.displayName = 'PopoverContent';
+
+export default PopoverContent;
---
+++ packages/react-styled-ui/src/Popover/PopoverTrigger.js
@@ -0,0 +1,77 @@
+import React, { Children, cloneElement, useRef } from 'react';
+import { usePopover } from './context';
+import Box from '../Box';
+
+const wrapEvent = (theirHandler, ourHandler) => event => {
+  theirHandler && theirHandler(event);
+  if (!event.defaultPrevented) {
+    return ourHandler(event);
+  }
+  return '';
+};
+
+const PopoverTrigger = ({ children }) => {
+  const {
+    referenceRef,
+    popoverId,
+    onToggle,
+    trigger,
+    onOpen,
+    isOpen,
+    onClose,
+    isHoveringRef,
+    showDelay,
+    hideDelay,
+  } = usePopover();
+  const _children = <Box display="inline-block">{children}</Box>; // always wrap a div to make sure the element can be bound event.
+  const child = Children.only(_children);
+  let eventHandlers = {};
+
+  if (trigger === 'click') {
+    eventHandlers = {
+      onClick: wrapEvent(child.props.onClick, onToggle),
+    };
+  }
+
+  const openTimeout = useRef(null);
+
+  if (trigger === 'hover') {
+    eventHandlers = {
+      onFocus: wrapEvent(child.props.onFocus, onOpen),
+      onKeyDown: wrapEvent(child.props.onKeyDown, event => {
+        if (event.key === 'Escape') {
+          setTimeout(onClose, hideDelay);
+        }
+      }),
+      onBlur: wrapEvent(child.props.onBlur, onClose),
+      onMouseEnter: wrapEvent(child.props.onMouseEnter, () => {
+        isHoveringRef.current = true;
+        openTimeout.current = setTimeout(onOpen, showDelay);
+      }),
+      onMouseLeave: wrapEvent(child.props.onMouseLeave, () => {
+        isHoveringRef.current = false;
+        if (openTimeout.current) {
+          clearTimeout(openTimeout.current);
+          openTimeout.current = null;
+        }
+        setTimeout(() => {
+          if (isHoveringRef.current === false) {
+            onClose();
+          }
+        }, hideDelay);
+      }),
+    };
+  }
+
+  return cloneElement(child, {
+    'aria-haspopup': 'dialog',
+    'aria-expanded': isOpen,
+    'aria-controls': popoverId,
+    ref: referenceRef,
+    ...eventHandlers,
+  });
+};
+
+PopoverTrigger.displayName = 'PopoverTrigger';
+
+export default PopoverTrigger;
