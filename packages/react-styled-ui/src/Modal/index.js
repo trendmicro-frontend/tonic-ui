4d78cd82587740f24c875a77ee925d27343b21ac
Roth <roth1002@gmail.com>
2020-05-05 01:06:35 +0800

    fix: remove overlay/content export

--- packages/react-styled-ui/src/Modal/index.js
+++ packages/react-styled-ui/src/Modal/index.js
@@ -1,26 +1,20 @@
 import Modal from './Modal';
 import ModalBody from './ModalBody';
-import ModalContent from './ModalContent';
 import ModalFooter from './ModalFooter';
 import ModalHeader from './ModalHeader';
-import ModalOverlay from './ModalOverlay';
 import ModalCloseButton from './ModalCloseButton';
 
 Modal.Header = ModalHeader;
 Modal.Body = ModalBody;
-Modal.Content = ModalContent;
 Modal.Footer = ModalFooter;
-Modal.Header = ModalHeader;
 Modal.Trigger = ModalOverlay;
 Modal.ModalCloseButton = ModalCloseButton;
 
 export {
   Modal,
   ModalBody,
-  ModalContent,
   ModalFooter,
   ModalHeader,
-  ModalOverlay,
   ModalCloseButton,
 };
 
