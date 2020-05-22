dda98e40f66e7356ebb1a56cd9f55170992ae732
Cheton Wu <cheton@gmail.com>
2020-05-22 23:38:43 +0800

    refactor: the effect of CSS ellipsis should not be controlled within the component itself

--- packages/react-styled-ui/src/Modal/ModalHeader.js
+++ packages/react-styled-ui/src/Modal/ModalHeader.js
@@ -8,18 +8,14 @@ const ModalHeader = forwardRef(({ children, hideCloseButton, ...restProps }, ref
   return (
     <Box
       ref={ref}
+      as="header"
       pl="6x"
       py="4x"
       pr="10x"
       id={headerId}
-      as="header"
       position="relative"
       fontSize="xl"
       lineHeight="xl"
-      whiteSpace="nowrap"
-      width="100%"
-      overflow="hidden"
-      textOverflow="ellipsis"
       {...restProps}
     >
       {children}
