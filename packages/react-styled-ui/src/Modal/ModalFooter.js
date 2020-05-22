99966eabd3582934cfed1d51c8d6278d81d14f0b
Cheton Wu <cheton@gmail.com>
2020-05-23 00:03:08 +0800

    refactor(ModalFooter): change vertical padding and add top border

--- packages/react-styled-ui/src/Modal/ModalFooter.js
+++ packages/react-styled-ui/src/Modal/ModalFooter.js
@@ -1,16 +1,27 @@
 import React, { forwardRef } from 'react';
 import Flex from '../Flex';
+import useColorMode from '../useColorMode';
 
-const ModalFooter = forwardRef((props, ref) => (
-  <Flex
-    ref={ref}
-    as="footer"
-    justify="flex-end"
-    px="6x"
-    py="3x"
-    {...props}
-  />
-));
+const ModalFooter = forwardRef((props, ref) => {
+  const { colorMode } = useColorMode();
+  const borderColor = {
+    dark: 'gray:80',
+    light: 'gray:80', // TBD: light mode is not ready yet
+  }[colorMode];
+
+  return (
+    <Flex
+      ref={ref}
+      as="footer"
+      justify="flex-end"
+      px="6x"
+      py="4x"
+      borderTop={1}
+      borderTopColor={borderColor}
+      {...props}
+    />
+  );
+});
 
 ModalFooter.displayName = 'ModalFooter';
 
