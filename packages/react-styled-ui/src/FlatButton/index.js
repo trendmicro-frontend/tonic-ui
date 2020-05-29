3fe6f3e59e28b5ccca9a52eb181bfd4b556ef53f
Cheton Wu <cheton@gmail.com>
2020-05-30 00:31:45 +0800

    chore: consistent indentation

--- packages/react-styled-ui/src/FlatButton/index.js
+++ packages/react-styled-ui/src/FlatButton/index.js
@@ -2,42 +2,40 @@ import React, { forwardRef } from 'react';
 import useButtonStyle from './styles';
 import ButtonBase from '../ButtonBase';
 
-const FlatButton = forwardRef(
-  (
-    {
-      as: Comp = 'button',
-      borderRadius = 'sm',
-      children,
-      px = '3x',
-      size = 'md',
-      type = 'button',
-      variant = 'solid',
-      variantColor,
-      ...rest
-    },
-    ref,
-  ) => {
-    const buttonStyleProps = useButtonStyle({
-      color: variantColor,
-      size,
-      variant,
-    });
-
-    return (
-      <ButtonBase
-        ref={ref}
-        as={Comp}
-        type={type}
-        borderRadius={borderRadius}
-        px={px}
-        {...buttonStyleProps}
-        {...rest}
-      >
-        { children }
-      </ButtonBase>
-    );
+const FlatButton = forwardRef((
+  {
+    as: Comp = 'button',
+    borderRadius = 'sm',
+    children,
+    px = '3x',
+    size = 'md',
+    type = 'button',
+    variant = 'solid',
+    variantColor,
+    ...rest
   },
-);
+  ref,
+) => {
+  const buttonStyleProps = useButtonStyle({
+    color: variantColor,
+    size,
+    variant,
+  });
+
+  return (
+    <ButtonBase
+      ref={ref}
+      as={Comp}
+      type={type}
+      borderRadius={borderRadius}
+      px={px}
+      {...buttonStyleProps}
+      {...rest}
+    >
+      { children }
+    </ButtonBase>
+  );
+});
 
 FlatButton.displayName = 'FlatButton';
 
