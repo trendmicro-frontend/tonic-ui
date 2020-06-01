361418c6916ba89abde146cb1923d95d833708d0
Tina C Lin <smalltase@gmail.com>
2020-06-01 19:03:39 +0800

    feat: add Tag component (#134)
    * feat: add Tag component
    
    * style: remove invalid style and adjust the position of close button
    
    * feat: add invalid props
    
    * docs: update docs for Tag
    
    * feat: add ActivedTag component
    
    * docs: update docs for Tag
    
    * chore: add component link
    
    * chore: remove ActivedTag component
    
    * refactor: refactor invalid props
    
    * chore: rename isCloseable props
    
    * docs: update tags component
    
    Co-authored-by: Cheton Wu <cheton@gmail.com>

---
+++ packages/react-styled-ui/src/Tag/index.js
@@ -0,0 +1,77 @@
+import React, { forwardRef } from 'react';
+import { useTagStyle, useTagCloseButtonStyle } from './styles';
+import Box from '../Box';
+import ButtonBase from '../ButtonBase';
+import Icon from '../Icon';
+import PseudoBox from '../PseudoBox';
+
+const TagCloseButton = ({ size, ...props }) => {
+  const closeButtonStyleProps = useTagCloseButtonStyle({ size });
+  return (
+    <ButtonBase {...closeButtonStyleProps} {...props} />
+  );
+};
+
+const Tag = forwardRef(
+  (
+    {
+      borderRadius = 'sm',
+      size = 'md',
+      variant = 'solid',
+      variantColor = 'gray',
+      isInvalid,
+      isCloseButtonVisible,
+      disabled,
+      children,
+      onClose,
+      ...rest
+    },
+    ref,
+  ) => {
+    const tagStyleProps = useTagStyle({
+      color: variantColor,
+      size,
+      variant,
+      canFocus: isCloseButtonVisible,
+      isCloseButtonVisible,
+    });
+
+    return (
+      <PseudoBox
+        ref={ref}
+        disabled={disabled}
+        aria-disabled={disabled}
+        aria-invalid={isInvalid}
+        borderRadius={borderRadius}
+        tabIndex="0"
+        {...tagStyleProps}
+        {...rest}
+      >
+        {/* This Box is for rendering background color of Tag. */}
+        <Box
+          borderRadius={borderRadius}
+          position="absolute"
+          top="0"
+          bottom="0"
+          left="0"
+          right="0"
+        />
+        {/* The z-index is for placing text over the above box. */}
+        <Box zIndex="1">{ children }</Box>
+        {!!isCloseButtonVisible && (
+          <TagCloseButton
+            size={size}
+            disabled={disabled}
+            onClick={onClose}
+          >
+            <Icon name="_core.close-s" />
+          </TagCloseButton>
+        )}
+      </PseudoBox>
+    );
+  },
+);
+
+Tag.displayName = 'Tag';
+
+export default Tag;
