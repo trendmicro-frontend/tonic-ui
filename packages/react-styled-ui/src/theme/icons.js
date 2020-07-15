808a0ebbce1739f7e56a82e79695371519eb1e09
Tina C Lin <smalltase@gmail.com>
2020-07-15 15:38:40 +0800

    chore: update checkbox icon after HIE reviewed (#181)
    * chore: update checkbox icon after HIE reviewed
    
    * style: adjust icon size after the icon is changed

--- packages/react-styled-ui/src/theme/icons.js
+++ packages/react-styled-ui/src/theme/icons.js
@@ -12,11 +12,12 @@ const icons = {
   },
   'check': {
     path: (
-      <g fill="currentColor">
-        <polygon points="5.5 11.9993304 14 3.49933039 12.5 2 5.5 8.99933039 1.5 4.9968652 0 6.49933039" />
-      </g>
+      <path
+        fill="currentColor"
+        d="M6 11.060l-3-3-1 1 4 4 9-9-1-1z"
+      />
     ),
-    viewBox: '0 0 14 14',
+    viewBox: '0 0 16 16',
   },
   'chevron-down': {
     path: (
