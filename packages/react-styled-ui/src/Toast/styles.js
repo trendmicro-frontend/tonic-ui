e59e1605298290ab73ecac1c2637de8809b484d4
Austin Hung <austinhung81@gmail.com>
2020-08-05 09:59:50 +0800

    docs: updated the theme wordings and color "black:emphasis" value changed (#206)
    * fix: change black:emphasis to black:primary
    
    * fix: change black:emphasis from 0.92 to 1.0
    
    * fix: edit H1, H2 fontWeight
    
    * docs: update wording
    
    * docs: update button and toast component wordings
    
    * docs: change color page wording
    
    Co-authored-by: Austin Hung <austin_h_hung@trendmicro.com>

--- packages/react-styled-ui/src/Toast/styles.js
+++ packages/react-styled-ui/src/Toast/styles.js
@@ -103,8 +103,8 @@ const useToastCloseButtonStyle = () => {
     light: 'black:tertiary',
   }[colorMode];
   const hoverColor = {
-    dark: 'black:emphasis',
-    light: 'black:emphasis',
+    dark: 'black:primary',
+    light: 'black:primary',
   }[colorMode];
   const activeColor = color;
   const focusColor = color;
