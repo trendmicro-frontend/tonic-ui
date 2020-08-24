ec3b741392f8b4affc753398e5dc5acc7ed53ab1
Tina C Lin <smalltase@gmail.com>
2020-08-24 12:13:53 +0800

    fix: checkbox icons (#219)

---
+++ packages/react-styled-ui/src/Checkbox/IconMinus.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import SVGIcon from '../SVGIcon';
+
+const IconMinus = (props) => {
+  const viewBox = '0 0 24 24';
+  return (
+    <SVGIcon viewBox={viewBox} {...props}>
+      <g fill="currentColor">
+        <rect
+          height="18"
+          width="18"
+          x="3"
+          y="3"
+        />
+      </g>
+    </SVGIcon>
+  );
+};
+
+export default IconMinus;
