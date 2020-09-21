3ef463a2f90a70f4f755d4823e3b0ac9e41834d2
Cheton Wu <chetonu@gmail.com>
2020-09-21 14:46:29 +0800

    feat: use `base` for `rem` unit and `fixed` for `px` unit

--- packages/styled-ui-theme/src/fixed.js
+++ packages/styled-ui-theme/src/fixed.js
@@ -1,19 +1,19 @@
-const spaceUnit = 'rem';
+const spaceUnit = 'px';
 const spaceDefinition = {
   quarter: {
     list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     notation: 'q',
-    value: 0.0625,
+    value: 1,
   },
   half: {
     list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     notation: 'h',
-    value: 0.125,
+    value: 2,
   },
   whole: {
     list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 24, 32, 40, 48, 56, 64],
     notation: 'x',
-    value: 0.25,
+    value: 4,
   },
 };
 
@@ -200,27 +200,27 @@ const fontWeights = {
 };
 
 const fontSizes = {
-  xs: '.75rem',
-  sm: '.875rem',
-  md: '1rem',
-  lg: '1.125rem',
-  xl: '1.25rem',
-  '2xl': '1.5rem',
-  '3xl': '1.75rem',
-  '4xl': '2rem',
+  xs: '12px',
+  sm: '14px',
+  md: '16px',
+  lg: '18px',
+  xl: '20px',
+  '2xl': '24px',
+  '3xl': '28px',
+  '4xl': '32px',
 };
 
 const lineHeights = {
   normal: 'normal',
   base: '1.5',
-  xs: '1.125rem',
-  sm: '1.25rem',
-  md: '1.375rem',
-  lg: '1.5rem',
-  xl: '1.75rem',
-  '2xl': '2rem',
-  '3xl': '2.25rem',
-  '4xl': '2.5rem',
+  xs: '18px',
+  sm: '20px',
+  md: '22px',
+  lg: '24px',
+  xl: '28px',
+  '2xl': '32px',
+  '3xl': '36px',
+  '4xl': '40px',
 };
 
 const letterSpacings = {
@@ -245,9 +245,9 @@ const borderWidths = {
 const radii = {
   circle: '50%',
   none: 0,
-  sm: '.1875rem',
-  md: '.375rem',
-  lg: '.75rem',
+  sm: '3px',
+  md: '6px',
+  lg: '12px',
 };
 
 const shadows = {
--- packages/styled-ui-theme/src/index.js
+++ packages/styled-ui-theme/src/index.js
@@ -1,7 +1,9 @@
 import base from './base';
+import fixed from './fixed';
 import getColorPalette from './color-palette';
 
 export {
   base,
+  fixed,
   getColorPalette,
 };
