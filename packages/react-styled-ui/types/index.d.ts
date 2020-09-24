ce35d3ceb6819c16ab79ea51f1160770246d2747
RossThomson <thomson360@gmail.com>
2020-09-24 11:38:10 +1000

    feat: Add typescript declaration file

---
+++ packages/react-styled-ui/types/index.d.ts
@@ -0,0 +1,513 @@
+declare module "@trendmicro/react-styled-ui" {
+  export import Alert = __StyledUI.__Alert.Alert;
+  export import Box = __StyledUI.__Box.Box;
+  export import Button = __StyledUI.__Button.Button;
+  export import ColorModeProvider = __StyledUI.__ColorModeProvider.ColorModeProvider;
+  export import CSSBaseline = __StyledUI.__CSSBaseline.CSSBaseline;
+  export import Divider = __StyledUI.__Divider.Divider;
+  export import Flex = __StyledUI.__Flex.Flex;
+  export import Grid = __StyledUI.__Grid.Grid;
+  export import Icon = __StyledUI.__Icon.Icon;
+  export import Image = __StyledUI.__Image.Image;
+  export import Input = __StyledUI.__Input.Input;
+  export import Link = __StyledUI.__Link.Link;
+  export import Modal = __StyledUI.__Modal.Modal;
+  export import ModalHeader = __StyledUI.__Modal.ModalHeader;
+  export import ModalOverlay = __StyledUI.__Modal.ModalOverlay;
+  export import ModalContent = __StyledUI.__Modal.ModalContent;
+  export import ModalBody = __StyledUI.__Modal.ModalBody;
+  export import ModalFooter = __StyledUI.__Modal.ModalFooter;
+  export import PseudoBox = __StyledUI.__PseudoBox.PseudoBox;
+  export import Stack = __StyledUI.__Stack.Stack;
+  export import Select = __StyledUI.__Input.Select;
+  export import Spinner = __StyledUI.__Spinner.Spinner;
+  export import SearchInput = __StyledUI.__Input.SearchInput;
+  export import Text = __StyledUI.__Text.Text;
+  export import Textarea = __StyledUI.__Input.Textarea;
+  export import TextLabel = __StyledUI.__TextLabel.TextLabel;
+  export import theme = __StyledUI.__Theme.theme;
+  export import ThemeProvider = __StyledUI.__Theme.ThemeProvider;
+  export import Tooltip = __StyledUI.__Tooltip.Tooltip;
+  export import useColorMode = __StyledUI.__useColorMode.useColorMode;
+}
+
+declare module "@trendmicro/react-styled-ui/Alert" {
+  import Alert = __StyledUI.__Alert.Alert;
+
+  export default Alert;
+}
+
+// https://trendmicro-frontend.github.io/styled-ui/box
+declare module "@trendmicro/react-styled-ui/Box" {
+  import Box = __StyledUI.__Box.Box;
+
+  export default Box;
+}
+
+declare module "@trendmicro/react-styled-ui/Button" {
+  import Button = __StyledUI.__Button.Button;
+
+  export default Button;
+}
+
+declare module "@trendmicro/react-styled-ui/ColorModeProvider" {
+  import ColorModeProvider = __StyledUI.__ColorModeProvider.ColorModeProvider;
+
+  export default ColorModeProvider;
+}
+
+// https://trendmicro-frontend.github.io/styled-ui/cssbaseline
+declare module "@trendmicro/react-styled-ui/CSSBaseline" {
+  import CSSBaseline = __StyledUI.__CSSBaseline.CSSBaseline;
+
+  export default CSSBaseline;
+}
+
+// https://trendmicro-frontend.github.io/styled-ui/divider
+declare module "@trendmicro/react-styled-ui/Divider" {
+  import Divider = __StyledUI.__Divider.Divider;
+
+  export default Divider;
+}
+
+// https://trendmicro-frontend.github.io/styled-ui/flex
+declare module "@trendmicro/react-styled-ui/Flex" {
+  import Flex = __StyledUI.__Flex.Flex;
+
+  export default Flex;
+}
+
+// https://trendmicro-frontend.github.io/styled-ui/grid
+declare module "@trendmicro/react-styled-ui/Grid" {
+  import Grid = __StyledUI.__Grid.Grid;
+
+  export default Grid;
+}
+
+// https://trendmicro-frontend.github.io/styled-ui/Image
+declare module "@trendmicro/react-styled-ui/Image" {
+  import Image = __StyledUI.__Image.Image;
+
+  export default Image;
+}
+
+declare module "@trendmicro/react-styled-ui/Input" {
+  import Input = __StyledUI.__Input.Input;
+
+  export default Input;
+}
+
+declare module "@trendmicro/react-styled-ui/Icon" {
+  import Icon = __StyledUI.__Icon.Icon;
+
+  export default Icon;
+}
+
+declare module "@trendmicro/react-styled-ui/Link" {
+  import Link = __StyledUI.__Link.Link;
+
+  export default Link;
+}
+
+declare module "@trendmicro/react-styled-ui/Modal" {
+  import Modal = __StyledUI.__Modal.Modal;
+
+  export default Modal;
+}
+
+declare module "@trendmicro/react-styled-ui/Modal/ModalHeader" {
+  import ModalHeader = __StyledUI.__Modal.ModalHeader;
+
+  export default ModalHeader;
+}
+
+declare module "@trendmicro/react-styled-ui/Modal/ModalOverlay" {
+  import ModalOverlay = __StyledUI.__Modal.ModalOverlay;
+
+  export default ModalOverlay;
+}
+
+declare module "@trendmicro/react-styled-ui/Modal/ModalContent" {
+  import ModalContent = __StyledUI.__Modal.ModalContent;
+
+  export default ModalContent;
+}
+
+declare module "@trendmicro/react-styled-ui/Modal/ModalBody" {
+  import ModalBody = __StyledUI.__Modal.ModalBody;
+
+  export default ModalBody;
+}
+
+declare module "@trendmicro/react-styled-ui/Modal/ModalFooter" {
+  import ModalFooter = __StyledUI.__Modal.ModalFooter;
+
+  export default ModalFooter;
+}
+
+declare module "@trendmicro/react-styled-ui/PseudoBox" {
+  import PseudoBox = __StyledUI.__PseudoBox.PseudoBox;
+
+  export default PseudoBox;
+}
+
+// https://trendmicro-frontend.github.io/styled-ui/stack
+declare module "@trendmicro/react-styled-ui/Stack" {
+  import Stack = __StyledUI.__Stack.Stack;
+
+  export default Stack;
+}
+
+declare module "@trendmicro/react-styled-ui/Select" {
+  import Select = __StyledUI.__Input.Select;
+
+  export default Select;
+}
+
+declare module "@trendmicro/react-styled-ui/Spinner" {
+  import Spinner = __StyledUI.__Spinner.Spinner;
+
+  export default Spinner;
+}
+
+declare module "@trendmicro/react-styled-ui/SearchInput" {
+  import SearchInput = __StyledUI.__Input.SearchInput;
+
+  export default SearchInput;
+}
+
+declare module "@trendmicro/react-styled-ui/Text" {
+  import Text = __StyledUI.__Text.Text;
+
+  export default Text;
+}
+
+declare module "@trendmicro/react-styled-ui/Textarea" {
+  import Textarea = __StyledUI.__Input.Textarea;
+
+  export default Textarea;
+}
+
+declare module "@trendmicro/react-styled-ui/TextLabel" {
+  import TextLabel = __StyledUI.__TextLabel.TextLabel;
+
+  export default TextLabel;
+}
+
+declare module "@trendmicro/react-styled-ui/theme" {
+  import theme = __StyledUI.__Theme.theme;
+
+  export default theme;
+}
+
+declare module "@trendmicro/react-styled-ui/ThemeProvider" {
+  import ThemeProvider = __StyledUI.__Theme.ThemeProvider;
+
+  export default ThemeProvider;
+}
+
+declare module "@trendmicro/react-styled-ui/Tooltip" {
+  import Tooltip = __StyledUI.__Tooltip.Tooltip;
+
+  export default Tooltip;
+}
+
+declare module "@trendmicro/react-styled-ui/useColorMode" {
+  import useColorMode = __StyledUI.__useColorMode.useColorMode;
+
+  export default useColorMode;
+}
+
+declare namespace __StyledUI {
+  type TypographySize =
+    | "xs"
+    | "sm"
+    | "md"
+    | "lg"
+    | "xl"
+    | "2xl"
+    | "3xl"
+    | "4xl";
+
+  type ColorTheme = "light" | "dark";
+
+  type IconProps = {
+    color?: string;
+    height?: string;
+    name: string;
+    path?: React.ReactNode;
+    size?: string;
+    viewBox?: string;
+    width?: string;
+  };
+
+  type BoxProps = {
+    align?: string;
+    alignItems?: string;
+    backgroundColor?: string;
+    border?: string;
+    borderColor?: string;
+    boxShadow?: string;
+    borderRadius?: string;
+    borderTop?: string;
+    borderBottom?: string;
+    boxSizing?: string;
+    color?: string;
+    children?: React.ReactNode;
+    display?: string;
+    fontFamily?: string;
+    fontSize?: TypographySize | string;
+    fontWeight?: string;
+    height?: string;
+    htmlFor?: string;
+    id?: string;
+    justifyContent?: string;
+    minHeight?: string;
+    left?: string;
+    lineHeight?: string;
+    margin?: string;
+    marginBottom?: string;
+    mb?: string;
+    mr?: string;
+    marginTop?: string;
+    marginLeft?: string;
+    marginRight?: string;
+    maxWidth?: string;
+    maxHeight?: string;
+    opacity?: string;
+    p?: string;
+    padding?: string;
+    paddingBottom?: string;
+    paddingLeft?: string;
+    paddingTop?: string;
+    paddingX?: string;
+    paddingY?: string;
+    position?: string;
+    px?: string;
+    right?: string;
+    size?: string;
+    top?: string;
+    textAlign?: string;
+    whiteSpace?: string;
+    width?: string;
+    zIndex?: string;
+  };
+
+  type Direction = "column" | "column-reverse" | "row" | "row-reverse";
+
+  type FlexProps = BoxProps & {
+    direction?: Direction;
+    display?: "flex";
+  };
+  export namespace __Alert {
+    type AlertProps = FlexProps & {
+      icon?: React.ReactNode;
+      isCloseButtonVisible?: boolean;
+      onClose?: () => void;
+      severity: string;
+    };
+    export const Alert: React.FC<AlertProps>;
+  }
+
+  export namespace __Box {
+    export const Box: React.FC<BoxProps>;
+  }
+
+  export namespace __Button {
+    type ButtonProps = BoxProps & {
+      disabled?: boolean;
+      onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+      onMouseEnter?: () => void;
+      type?: string;
+      variant?: string;
+    };
+    export const Button: React.FC<ButtonProps>;
+  }
+  export namespace __ColorModeProvider {
+    type ColorModeProps = {
+      value: ColorTheme;
+    };
+
+    export const ColorModeProvider: React.FC<ColorModeProps>;
+  }
+
+  export namespace __CSSBaseline {
+    type CSSBaselineProps = {};
+
+    const CSSBaseline: React.FC<CSSBaselineProps>;
+  }
+  export namespace __Divider {
+    type DividerProps = {
+      vertical?: boolean;
+    };
+
+    export const Divider: React.FC<DividerProps>;
+  }
+  export namespace __Flex {
+    export const Flex: React.FC<FlexProps>;
+  }
+  export namespace __Grid {
+    type GridProps = BoxProps & {
+      display?: "grid";
+      gap?: string;
+      templateColumns?: string;
+    };
+
+    export const Grid: React.FC<GridProps>;
+  }
+  export namespace __Image {
+    type ImageProps = BoxProps & {
+      src: string;
+      bg?: string;
+    };
+    export const Image: React.FC<ImageProps>;
+  }
+  export namespace __Input {
+    type InputProps = BoxProps & {
+      defaultValue?: string | number | undefined | null;
+      disabled?: boolean;
+      label?: string;
+      name?: string;
+      onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+      onKeyPress?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+      onMouseEnter?: () => void;
+      placeholder?: string;
+      ref?: React.RefObject;
+      required?: boolean;
+      type?: string;
+      value?: string | number | undefined | null;
+    };
+    export const Input: React.FC<InputProps>;
+
+    type SelectProps = InputProps & {
+      children: React.ReactNode;
+    };
+    export const Select: React.FC<SelectProps>;
+
+    type SearchInputProps = InputProps & {
+      size?: string;
+    };
+    export const SearchInput: React.FC<SearchInputProps>;
+
+    type TextareaProps = InputProps & {
+      children?: React.ReactNode;
+      rows?: number;
+      resize?: "none" | "both" | "horizontal" | "vertical";
+    };
+    export const Textarea: React.FC<TextareaProps>;
+  }
+  export namespace __Icon {
+    export const Icon: React.FC<IconProps>;
+  }
+  export namespace __Link {
+    type LinkProps = {
+      children: string;
+      href?: string;
+      isSelected?: boolean;
+      onClick?: (e: React.MouseEvent<HTMLElement>) => void;
+    };
+    export const Link: React.FC<LinkProps>;
+  }
+  export namespace __Modal {
+    type ModalProps = {
+      autoFocus?: boolean;
+      isCloseButtonVisible?: boolean;
+      isOpen?: boolean;
+      onClose?: () => void;
+      size?: string;
+    };
+    export const Modal: React.FC<ModalProps>;
+
+    type ModalHeaderProps = BoxProps & { title?: string };
+    export const ModalHeader: React.FC<ModalHeaderProps>;
+
+    type ModalOverlayProps = BoxProps;
+    export const ModalOverlay: React.FC<ModalOverlayProps>;
+
+    type ModalContentProps = BoxProps;
+    export const ModalContent: React.FC<ModalContentProps>;
+
+    type ModalBodyProps = BoxProps;
+    export const ModalBody: React.FC<ModalBodyProps>;
+
+    type ModalFooterProps = BoxProps;
+    export const ModalFooter: React.FC<ModalFooterProps>;
+  }
+
+  export namespace __PseudoBox {
+    type PseudoBoxProps = {
+      as: React.ReactNode;
+      children?: React.ReactNode;
+      _hover?: { color: string };
+      name?: string;
+    };
+
+    export const PseudoBox: React.FC<PseudoBoxProps>;
+  }
+
+  export namespace __Stack {
+    type StackProps = FlexProps & {
+      spacing?: number | string;
+    };
+    export const Stack: React.FC<StackProps>;
+  }
+
+  export namespace __Spinner {
+    type SpinnerProps = {
+      color?: string;
+      size?: string;
+      strokeWidth?: number;
+      speed?: number;
+    };
+
+    export const Spinner: React.FC<SpinnerProps>;
+  }
+  export namespace __Text {
+    type TextProps = BoxProps;
+
+    export const Text: React.FC<TextProps>;
+  }
+
+  export namespace __TextLabel {
+    type TextLabelProps = BoxProps;
+
+    export const TextLabel: React.FC<TextLabelProps>;
+  }
+
+  export namespace __Theme {
+    type Theme = {
+      fontSizes?: {
+        xs: string;
+        sm: string;
+        md: string;
+        lg: string;
+        xl: string;
+        "2xl": string;
+        "3xl": string;
+        "4xl": string;
+      };
+      icons: Record<IconProps, string>;
+    };
+    export const theme: Theme;
+
+    type ThemeProviderProps = {
+      theme?: Theme;
+    };
+
+    export const ThemeProvider: React.FC<ThemeProviderProps>;
+  }
+
+  export namespace __Tooltip {
+    type TooltipProps = {
+      children: React.ReactNode;
+      label: string;
+      placement?: string;
+    };
+
+    export const Tooltip: React.FC<TooltipProps>;
+  }
+
+  export namespace __useColorMode {
+    export const useColorMode: () => { colorMode: ColorTheme };
+  }
+}
