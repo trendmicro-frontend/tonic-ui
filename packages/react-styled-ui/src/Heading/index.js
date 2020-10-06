26e514372119c2763d770d90ce499b6177822bea
“Austin <austinhung81@gmail.com>
2020-10-06 17:47:52 +0800

    fix: heading component property

--- packages/react-styled-ui/src/Heading/index.js
+++ packages/react-styled-ui/src/Heading/index.js
@@ -1,75 +1,64 @@
 import React, { forwardRef } from 'react';
 import Box from '../Box';
-import useTheme from '../useTheme';
 
-const defaultVariantMapping = {
-  h1: {
-    as: 'h1',
-    fontSize: '4xl',
-    lineHeight: '4xl',
+const defaultTypeScaleMapping = {
+  heading1: {
+    fontSize: 'sm',
+    lineHeight: 'sm',
     fontWeight: 'semibold',
-    margin: 0,
   },
-  h2: {
-    as: 'h2',
-    fontSize: '2xl',
-    lineHeight: '2xl',
-    fontWeight: 'semibold',
-    margin: 0,
-  },
-  h3: {
-    as: 'h3',
-    fontSize: 'xl',
-    lineHeight: 'xl',
-    fontWeight: 'semibold',
-    margin: 0,
-  },
-  h4: {
-    as: 'h4',
+  heading2: {
     fontSize: 'md',
     lineHeight: 'md',
     fontWeight: 'semibold',
-    margin: 0,
   },
-  h5: {
-    as: 'h5',
-    fontSize: 'sm',
-    lineHeight: 'sm',
+  heading3: {
+    fontSize: 'lg',
+    lineHeight: 'lg',
     fontWeight: 'semibold',
-    margin: 0,
   },
-  h6: {
-    as: 'h6',
-    fontSize: 'sm',
-    lineHeight: 'sm',
+  heading4: {
+    fontSize: 'xl',
+    lineHeight: 'xl',
     fontWeight: 'normal',
-    margin: 0,
+  },
+  heading5: {
+    fontSize: 'xl',
+    lineHeight: 'xl',
+    fontWeight: 'semibold',
+  },
+  heading6: {
+    fontSize: '2xl',
+    lineHeight: '2xl',
+    fontWeight: 'normal',
+  },
+  heading7: {
+    fontSize: '3xl',
+    lineHeight: '3xl',
+    fontWeight: 'normal',
+  },
+  heading8: {
+    fontSize: '4xl',
+    lineHeight: '4xl',
+    fontWeight: 'normal',
   },
 };
 
 const Heading = forwardRef((
   {
-    size,
-    variant,
+    typeScale,
     ...rest
   },
   ref
 ) => {
-  const { fontSizes } = useTheme();
-  const sizeProps = {};
-  if ((size !== undefined) && Object.prototype.hasOwnProperty.call(fontSizes, size)) {
-    sizeProps.fontSize = size;
-    sizeProps.lineHeight = size;
-  }
-  const variantProps = defaultVariantMapping[variant];
+  const typeScaleProps = defaultTypeScaleMapping[typeScale];
 
   return (
     <Box
       ref={ref}
       display="block"
       fontFamily="heading"
-      {...sizeProps}
-      {...variantProps}
+      {...typeScaleProps}
       {...rest}
     />
   );
