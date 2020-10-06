6833dc9267475dac356f6099b69cd0ce8e9ca204
tina_c_lin <tina_c_lin@trendmicro.com>
2020-10-06 11:55:51 +0800

    docs: add the example for movable table

--- packages/styled-ui-docs/components/CodeBlock.jsx
+++ packages/styled-ui-docs/components/CodeBlock.jsx
@@ -6,6 +6,7 @@ import React, { useCallback, useState } from 'react';
 import { Scrollbars } from 'react-custom-scrollbars';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 import Lorem from 'react-lorem-component';
+import * as ReactMovable from 'react-movable';
 import * as ReactTable from 'react-table';
 import { AutoSizer } from 'react-virtualized';
 import { codeBlockLight, codeBlockDark } from '../prism-themes/styled-ui';
@@ -17,6 +18,7 @@ import useToast from './useToast';
 const ThirdPartyComponents = {
   AutoSizer,
   Scrollbars,
+  ...ReactMovable,
   ...ReactTable,
 };
 const CustomedComponents = {
