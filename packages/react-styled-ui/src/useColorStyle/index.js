ccc3feefb35b9a2e3542c05f3b899d9583ad2264
Cheton Wu <cheton@gmail.com>
2021-01-05 11:52:32 +0800

    refactor: refine useColorStyle Hook

--- packages/react-styled-ui/src/useColorStyle/index.js
+++ packages/react-styled-ui/src/useColorStyle/index.js
@@ -1,10 +1,6 @@
 import { ensurePlainObject } from 'ensure-type';
-import _get from 'lodash/get';
-import _includes from 'lodash/includes';
 import { useContext } from 'react';
 import { ColorStyleContext } from '../ColorStyleProvider';
-
-const colorModes = ['dark', 'light'];
 
 const useColorStyle = (options) => {
   const { colorMode } = { ...options };
@@ -13,20 +9,23 @@ const useColorStyle = (options) => {
     throw new Error('The `useContext` hook is not available with your React version.');
   }
 
-  const { colorStyle, setColorStyle } = useContext(ColorStyleContext);
+  let { colorStyle, setColorStyle } = useContext(ColorStyleContext);
   if (colorStyle === undefined) {
     throw new Error('The `useColorStyle` hook must be called from a descendent of the `ColorStyleProvider`.');
   }
 
-  if (!_includes(colorModes, colorMode)) {
-    throw new Error('The `colorMode` must be one of:', colorModes.join(', '));
+  colorStyle = ensurePlainObject(colorStyle);
+  if (!colorStyle.hasOwnProperty(colorMode)) {
+    throw new Error('The `colorMode` must be one of:', Object.keys(colorStyle));
   }
 
-  const getter = ensurePlainObject(_get(colorStyle, [colorMode]));
+  const getter = ensurePlainObject(colorStyle[colorMode]);
   const setter = (value) => {
     setColorStyle(prevState => {
+      prevState = ensurePlainObject(prevState);
+
       if (typeof value === 'function') {
-        value = value(ensurePlainObject(_get(prevState, [colorMode])));
+        value = value(ensurePlainObject(prevState[colorMode]));
       }
 
       return {
