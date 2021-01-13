e7c90eca8a03d4e0a9b340308a5538728f6bfa21
Cheton Wu <cheton@gmail.com>
2021-01-13 22:38:35 +0800

    feat: enable website tracking using Google Analytics (#267)
    * feat: enable website tracking with Google Analytics
    
    * chore: add code comment
    
    * chore: set Universal Analytics tracking ID

--- packages/styled-ui-docs/pages/_app.js
+++ packages/styled-ui-docs/pages/_app.js
@@ -12,10 +12,16 @@ import {
 import App from 'next/app';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import MDXComponents from '../components/MDXComponents';
 import SideNav from '../components/SideNav';
+
+const pageview = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
 
 const customTheme = {
   ...theme,
@@ -74,6 +80,26 @@ const CustomApp = (props) => {
   useEffect(() => {
     router.pathname === '/' && router.push(`${process.env.PUBLIC_URL}/getting-started`);
   }, [router]);
+
+  // https://github.com/vercel/next.js/blob/canary/examples/with-react-ga/pages/_app.js
+  useEffect(() => {
+    ReactGA.initialize(process.env.GA_TRACKING_ID);
+    // `routeChangeComplete` won't run for the first page load unless the query string is
+    // hydrated later on, so here we log a page view if this is the first render and
+    // there's no query string
+    if (!router.asPath.includes('?')) {
+      pageview();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    // Listen for page changes after a navigation or when the query changes
+    router.events.on('routeChangeComplete', pageview);
+    return () => {
+      router.events.off('routeChangeComplete', pageview);
+    };
+  }, [router.events]);
 
   return (
     <ThemeProvider theme={customTheme}>
