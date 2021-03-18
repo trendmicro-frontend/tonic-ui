b61f7e475af748688bca83b326d117b471f607cf
WU, FENG-CHIN <nikeorz+github@gmail.com>
2021-03-18 17:39:41 +0800

    fix: uncaught Error: [ThemeProvider] Please make your theme prop a plain object (#280)
    Co-authored-by: Gino Wu <gino_wu@trendmicro.com>

--- packages/react-styled-ui/src/ThemeProvider/index.js
+++ packages/react-styled-ui/src/ThemeProvider/index.js
@@ -1,6 +1,6 @@
 import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming';
 import React from 'react';
-import { theme } from '../theme';
+import theme from '../theme';
 
 const ThemeProvider = ({
   theme: customTheme = theme,
