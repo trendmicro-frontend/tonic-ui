3a121f20cf3dc82a32feb9770981ddabdf2d9a4f
Cheton Wu <cheton@gmail.com>
2021-05-02 01:15:44 +0800

    feat: add sharp easing

--- packages/react-styled-ui/src/Transitions/transitions.js
+++ packages/react-styled-ui/src/Transitions/transitions.js
@@ -22,6 +22,8 @@ export const transitionEasing = {
   easeOut: 'cubic-bezier(0.0, 0, 0.2, 1)',
   // Objects leave the screen at full velocity. They do not decelerate when off-screen.
   easeIn: 'cubic-bezier(0.4, 0, 1, 1)',
+  // The sharp curve is used by objects that may return to the screen at any time.
+  sharp: 'cubic-bezier(0.4, 0, 0.6, 1)',
 };
 
 export const getEnterTransitionProps = ({
