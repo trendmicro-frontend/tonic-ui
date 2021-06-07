3283e228e3469b4eaff8de043e63ec3a7fadb088
Cheton Wu <cheton@gmail.com>
2021-06-07 10:56:47 +0800

    feat: migrating to Emotion 11 (#318)
    * chore: bump emotion to v11
    
    * chore: first attempt of migrating to Emotion 11

--- packages/styled-ui-docs/components/AnimatedCubeDemo.jsx
+++ packages/styled-ui-docs/components/AnimatedCubeDemo.jsx
@@ -1,4 +1,4 @@
-import { keyframes } from '@emotion/core';
+import { keyframes } from '@emotion/react';
 import { Box, Image, Stack, Text, Icon, useColorMode } from '@trendmicro/react-styled-ui';
 import React from 'react';
 import Cube from './Cube';
