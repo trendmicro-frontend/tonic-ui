931b2eafe458594baca4c5ea2aa395ab990165ee
Cheton Wu <cheton@gmail.com>
2021-06-17 21:02:58 +0800

    chore: work in progress

--- packages/react-styled-ui/src/ToastProvider/ToastContainer.js
+++ packages/react-styled-ui/src/ToastProvider/ToastContainer.js
@@ -8,6 +8,7 @@ const ToastContainer = forwardRef(({
   ...props
 }, ref) => {
   const styleProps = {
+    boxSizing: 'border-box',
     maxHeight: '100%',
     maxWidth: '100%',
     overflow: 'hidden',
@@ -18,26 +19,27 @@ const ToastContainer = forwardRef(({
 
   placement = ensureString(placement);
 
-  if (placement === 'top' || placement === 'bottom') {
-    styleProps.margin = '0 auto';
-    styleProps.textAlign = 'center';
-  }
   if (placement.includes('top')) {
     styleProps.top = 0;
   }
   if (placement.includes('bottom')) {
     styleProps.bottom = 0;
   }
-  if (!placement.includes('left')) {
+  if (placement.includes('left')) {
+    styleProps.left = 0;
+  }
+  if (placement.includes('right')) {
     styleProps.right = 0;
   }
-  if (!placement.includes('right')) {
-    styleProps.left = 0;
+  if (placement.includes('center') || placement === 'top' || placement === 'bottom') {
+    styleProps.left = '50%';
+    styleProps.transform = 'translateX(-50%)';
   }
 
   return (
     <Box
       ref={ref}
+      data-toast-placement={placement}
       {...styleProps}
       {...props}
     />
