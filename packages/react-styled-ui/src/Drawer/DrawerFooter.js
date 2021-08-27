454b7b8c9c93e5d93d6f2a721303f73ccc7873e3
Cheton Wu <cheton@gmail.com>
2021-08-27 15:12:31 +0800

    feat(drawer): update drawer's style in accordance with the visual spec (HUD-137) (#337)
    * feat(drawer): update drawer's style in accordance with the visual spec (hud-137)
    * fix: fix a bug that drawer content will cut off in the full width mode

--- packages/react-styled-ui/src/Drawer/DrawerFooter.js
+++ packages/react-styled-ui/src/Drawer/DrawerFooter.js
@@ -1,11 +1,17 @@
 import React, { forwardRef } from 'react';
 import PseudoBox from '../PseudoBox';
+import { useDrawer } from './context';
 import {
   useDrawerFooterStyle,
 } from './styles';
 
 const DrawerFooter = forwardRef((props, ref) => {
-  const styleProps = useDrawerFooterStyle();
+  const context = useDrawer(); // context might be an undefined value
+  const {
+    placement,
+  } = { ...context };
+  const styleProps = useDrawerFooterStyle({ placement });
+
   return (
     <PseudoBox
       ref={ref}
