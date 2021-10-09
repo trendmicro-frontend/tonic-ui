d9de0e74d5cd9ff6edd0907726412e9852007a5a
Cheton Wu <cheton@gmail.com>
2021-10-09 20:58:13 +0800

    feat: pass name prop to descendant checkboxes of the checkbox group

--- packages/react-styled-ui/src/CheckboxGroup/index.js
+++ packages/react-styled-ui/src/CheckboxGroup/index.js
@@ -1,6 +1,8 @@
 import { ensureArray } from 'ensure-type';
 import memoize from 'micro-memoize';
 import React, { useEffect, useState } from 'react';
+import config from '../shared/config';
+import { useId } from '../utils/autoId';
 import { CheckboxGroupProvider } from './context';
 
 const getMemoizedState = memoize(state => ({ ...state }));
@@ -9,19 +11,31 @@ const CheckboxGroup = ({
   children,
   defaultValue,
   disabled,
+  name,
   size,
   value: valueProp,
   variantColor,
   onChange,
 }) => {
+  const defaultId = useId();
+  name = name ?? `${config.name}:checkbox-${defaultId}`;
+
   const [state, setState] = useState({
     value: ensureArray(valueProp ?? defaultValue),
   });
+
+  useEffect(() => {
+    if (valueProp !== undefined) {
+      setState({ value: ensureArray(valueProp) });
+    }
+  }, [valueProp]);
+
   const handleChange = event => {
     const checkbox = {
       checked: event.target.checked,
       value: event.target.value,
     };
+
     const nextValue = !!(checkbox.checked)
       ? state.value.concat(ensureArray(checkbox.value)) // expect value to be neither null nor undefined
       : state.value.filter(v => (v !== checkbox.value)); // filter out unchecked values
@@ -37,14 +51,9 @@ const CheckboxGroup = ({
     }
   };
 
-  useEffect(() => {
-    if (valueProp !== undefined) {
-      setState({ value: ensureArray(valueProp) });
-    }
-  }, [valueProp]);
-
   const checkboxGroupState = getMemoizedState({
     disabled,
+    name,
     onChange: handleChange,
     size,
     value: state.value,
