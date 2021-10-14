9ab95c53757d4b440ce4e367fc5d9c70dbd14e53
Cheton Wu <cheton@gmail.com>
2021-10-14 23:14:11 +0800

    feat: add spin animation prop to the Icon component

--- packages/react-styled-ui/src/Icon/index.js
+++ packages/react-styled-ui/src/Icon/index.js
@@ -1,19 +1,43 @@
+import { keyframes } from '@emotion/react';
+import { ensurePlainObject } from 'ensure-type';
 import _get from 'lodash.get';
 import React, { forwardRef } from 'react';
 import SVGIcon from '../SVGIcon';
 import useTheme from '../useTheme';
 
+const spinAnimation = keyframes`
+  0% {
+      transform: rotate(0deg)
+  }
+  to {
+      transform: rotate(1turn)
+  }
+`;
+
 const Icon = forwardRef((
   {
     icon,
+    spin,
     ...rest
   },
   ref
 ) => {
   const { icons = {} } = useTheme();
-  const { path, ...restIconProps } = { ..._get(icons, icon, _get(icons, `tmicon-${icon}`)) };
+  const tmicon = _get(icons, [`tmicon-${icon}`]);
+  const { path, ...restIconProps } = ensurePlainObject(_get(icons, icon, tmicon));
+  const styleProps = {};
+
+  if (spin) {
+    styleProps['animation'] = `${spinAnimation} 2s linear infinite`;
+  };
+
   return (
-    <SVGIcon ref={ref} {...restIconProps} {...rest}>
+    <SVGIcon
+      ref={ref}
+      {...styleProps}
+      {...restIconProps}
+      {...rest}
+    >
       {path}
     </SVGIcon>
   );
