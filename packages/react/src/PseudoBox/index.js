0ca6273742bc744a32d1a878939509d31672099e
Cheton Wu <cheton@gmail.com>
2021-11-08 14:56:26 +0800

    chore: add deprecation warning message for PseudoBox

--- packages/react/src/PseudoBox/index.js
+++ packages/react/src/PseudoBox/index.js
@@ -1,7 +1,15 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useEffect } from 'react';
 import Box from '../Box';
 
-const PseudoBox = forwardRef((props, ref) => <Box ref={ref} {...props} />);
+const PseudoBox = forwardRef((props, ref) => {
+  useEffect(() => {
+    console.error('Warning: PseudoBox is deprecated and will be removed in a future release. Please use the Box component instead.');
+  }, []);
+
+  return (
+    <Box ref={ref} {...props} />
+  );
+});
 
 PseudoBox.displayName = 'PseudoBox';
 
