dda051bf32bf4ff3300efd11da97458c1c929fce
Cheton Wu <cheton@gmail.com>
2021-11-27 18:13:13 +0800

    chore: define a DOCUMENT_NODE (9) constant

--- packages/react/src/utils/dom/getWindow.js
+++ packages/react/src/utils/dom/getWindow.js
@@ -1,11 +1,12 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
+const DOCUMENT_NODE = 9;
+
 const getWindow = (node) => {
   if (node === node?.window) {
     return node;
   }
 
-  // https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
-  // The node type 9 is a `Document` node
-  return node?.nodeType === 9 ? node?.defaultView || node?.parentWindow : null;
+  return node?.nodeType === DOCUMENT_NODE ? node?.defaultView || node?.parentWindow : null;
 };
 
 export default getWindow;
