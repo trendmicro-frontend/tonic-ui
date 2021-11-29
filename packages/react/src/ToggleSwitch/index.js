de3ba07e87365a6a8024ae717a83c52ccb71b7b1
Cheton Wu <cheton@gmail.com>
2021-11-29 23:46:32 +0800

    chore: add deprecation message for ToggleSwitch

--- packages/react/src/ToggleSwitch/index.js
+++ packages/react/src/ToggleSwitch/index.js
@@ -1,7 +1,15 @@
 import React, { forwardRef } from 'react';
 import Switch from '../Switch';
 
-const ToggleSwitch = forwardRef((props, ref) => <Switch ref={ref} {...props} />);
+const ToggleSwitch = forwardRef((props, ref) => {
+  useEffect(() => {
+    console.error('Warning: ToggleSwitch is deprecated and will be removed in a future release. Please use the Switch component instead.');
+  }, []);
+
+  return (
+    <Switch ref={ref} {...props} />
+  );
+});
 
 ToggleSwitch.displayName = 'ToggleSwitch';
 
