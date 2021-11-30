222bbe04ee7a0ddb6a11aed8f0a07cac59df2ee2
Cheton Wu <cheton@gmail.com>
2021-11-30 21:57:37 +0800

    chore: add warning message for the removed activateOnKeypress prop

--- packages/react/src/Tabs/Tabs.js
+++ packages/react/src/Tabs/Tabs.js
@@ -1,113 +1,120 @@
 import React, {
   forwardRef,
   useState,
-  useRef
+  useRef,
 } from 'react';
 import { TabContext } from './context';
 import Box from '../Box';
+import useEffectOnce from '../hooks/useEffectOnce';
 import { useId } from '../utils/autoId';
+import warnRemovedProps from '../utils/warnRemovedProps';
 
-const Tabs = forwardRef(
-  (
-    {
-      children,
-      onChange,
-      index: controlledIndex,
-      defaultIndex,
-      isManual,
-      variant = 'line',
-      align = 'left',
-      size = 'md',
-      orientation = 'horizontal',
-      isFitted,
-      ...props
-    },
-    ref,
-  ) => {
-    const { current: isControlled } = useRef(controlledIndex != null);
-    const selectedPanelRef = useRef();
-
-    const getInitialIndex = () => {
-      if (!isManual) {
-        return defaultIndex || 0;
-      } else {
-        return controlledIndex || defaultIndex || 0;
-      }
-    };
-
-    const getActualIdx = () => {
-      if (isManual) {
-        return selectedIndex;
-      } else {
-        return isControlled ? controlledIndex : selectedIndex;
-      }
-    };
-
-    const [selectedIndex, setSelectedIndex] = useState(getInitialIndex);
-    const [manualIndex, setManualIndex] = useState(
-      controlledIndex || defaultIndex || 0,
-    );
-
-    let actualIdx = getActualIdx();
-    let manualIdx = isControlled ? controlledIndex : manualIndex;
-
-    const onChangeTab = index => {
-      if (!isControlled) {
-        setSelectedIndex(index);
-      }
-
-      if (isControlled && isManual) {
-        setSelectedIndex(index);
-      }
-
-      if (!isManual) {
-        onChange && onChange(index);
-      }
-    };
-
-    const onManualTabChange = index => {
-      if (!isControlled) {
-        setManualIndex(index);
-      }
-
-      if (isManual) {
-        onChange && onChange(index);
-      }
-    };
-
-    const onFocusPanel = () => {
-      if (selectedPanelRef.current) {
-        selectedPanelRef.current.focus();
-      }
-    };
-
-    const id = useId();
-
-    const context = {
-      id,
-      index: actualIdx,
-      manualIndex: manualIdx,
-      onManualTabChange,
-      isManual,
-      onChangeTab,
-      selectedPanelRef,
-      onFocusPanel,
-      size,
-      align,
-      variant,
-      isFitted,
-      orientation,
-    };
-
-    return (
-      <TabContext.Provider value={context}>
-        <Box ref={ref} {...props}>
-          {children}
-        </Box>
-      </TabContext.Provider>
-    );
+const Tabs = forwardRef((
+  {
+    activateOnKeypress, // deprecated
+    children,
+    onChange,
+    index: controlledIndex,
+    defaultIndex,
+    isManual,
+    variant = 'line',
+    align = 'left',
+    size = 'md',
+    orientation = 'horizontal',
+    isFitted,
+    ...props
   },
-);
+  ref,
+) => {
+  useEffectOnce(() => {
+    if (activateOnKeypress !== undefined) {
+      warnRemovedProps('activateOnKeypress');
+    }
+  });
+
+  const { current: isControlled } = useRef(controlledIndex != null);
+  const selectedPanelRef = useRef();
+
+  const getInitialIndex = () => {
+    if (!isManual) {
+      return defaultIndex || 0;
+    } else {
+      return controlledIndex || defaultIndex || 0;
+    }
+  };
+
+  const getActualIdx = () => {
+    if (isManual) {
+      return selectedIndex;
+    } else {
+      return isControlled ? controlledIndex : selectedIndex;
+    }
+  };
+
+  const [selectedIndex, setSelectedIndex] = useState(getInitialIndex);
+  const [manualIndex, setManualIndex] = useState(
+    controlledIndex || defaultIndex || 0,
+  );
+
+  let actualIdx = getActualIdx();
+  let manualIdx = isControlled ? controlledIndex : manualIndex;
+
+  const onChangeTab = index => {
+    if (!isControlled) {
+      setSelectedIndex(index);
+    }
+
+    if (isControlled && isManual) {
+      setSelectedIndex(index);
+    }
+
+    if (!isManual) {
+      onChange && onChange(index);
+    }
+  };
+
+  const onManualTabChange = index => {
+    if (!isControlled) {
+      setManualIndex(index);
+    }
+
+    if (isManual) {
+      onChange && onChange(index);
+    }
+  };
+
+  const onFocusPanel = () => {
+    if (selectedPanelRef.current) {
+      selectedPanelRef.current.focus();
+    }
+  };
+
+  const id = useId();
+
+  const context = {
+    id,
+    index: actualIdx,
+    manualIndex: manualIdx,
+    onManualTabChange,
+    isManual,
+    onChangeTab,
+    selectedPanelRef,
+    onFocusPanel,
+    size,
+    align,
+    variant,
+    isFitted,
+    orientation,
+  };
+
+  return (
+    <TabContext.Provider value={context}>
+      <Box ref={ref} {...props}>
+        {children}
+      </Box>
+    </TabContext.Provider>
+  );
+});
 
 Tabs.displayName = 'Tabs';
 
