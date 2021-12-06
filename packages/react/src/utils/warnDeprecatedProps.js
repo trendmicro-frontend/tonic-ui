915e30f17cf90b790a6d0b85ab816eb7381c12a6
Cheton Wu <cheton@gmail.com>
2021-12-06 19:29:09 +0800

    feat: refine `Badge` component and deprecate unwanted props (#395)
    * docs: deprecate the  prop
    
    * chore: add warnDeprecatedProps
    
    * chore: add useEffectOnce Hook
    
    * chore: work in progress
    
    * docs: update badge.mdx
    
    * chore: add `display: inline-flex` to the useBadgeStyle Hook
    
    * docs: update badge.mdx
    
    * chore: add warnRemovedProps
    
    * feat: add dot badge, the placement prop, and update badge examples
    
    * chore: add deprecation warning message to deprecated props
    
    * docs: update badge.mdx
    
    * docs: update badge.mdx
    
    * chore: use boxShadow to display the outline effect
    
    * fix: fix incorrect boxShadow color in light mode
    
    Co-authored-by: Cheton Wu <chetonu@gmail.com>

---
+++ packages/react/src/utils/warnDeprecatedProps.js
@@ -0,0 +1,47 @@
+import { ensureArray, ensureBoolean, ensureString } from 'ensure-type';
+
+const joinWords = (words) => {
+  words = ensureArray(words);
+  if (words.length === 0) {
+    return '';
+  }
+  if (words.length === 1) {
+    return `'${words[0]}'`;
+  }
+  if (words.length === 2) {
+    return `'${words[0]}' and '${words[1]}'`;
+  }
+  return `'${words.slice(0, -1).join('\', \'')}', and '${words.slice(-1)}'`;
+};
+
+const warnDeprecatedProps = (props, options) => {
+  const alternative = ensureArray(options?.alternative);
+  const willRemove = ensureBoolean(options?.willRemove);
+  const message = ensureString(options?.message);
+
+  props = ensureArray(props);
+  if (props.length === 0) {
+    return;
+  }
+
+  const messages = [];
+  const verb = (props.length > 1) ? 'are' : 'is';
+
+  if (willRemove) {
+    messages.push(`Warning: ${joinWords(props)} ${verb} deprecated and will be removed in the next major release.`);
+  } else {
+    messages.push(`Warning: ${joinWords(props)} ${verb} deprecated.`);
+  }
+
+  if (alternative.length > 0) {
+    messages.push(`Use ${joinWords(alternative)} instead.`);
+  }
+
+  if (message) {
+    messages.push(message);
+  }
+
+  console.error(messages.join(' '));
+};
+
+export default warnDeprecatedProps;
