72a1671d3d19c7b69d996c6e9b4310c6d3a268be
cheton <cheton@gmail.com>
2021-12-24 16:03:03 +0800

    feat: move DarkMode and LightMode components to the ColorMode folder

--- packages/react/src/ColorMode/index.js
+++ packages/react/src/ColorMode/index.js
@@ -1,7 +1,11 @@
 import ColorModeProvider from './ColorModeProvider';
+import DarkMode from './DarkMode';
+import LightMode from './LightMode';
 import useColorMode from './useColorMode';
 
 export {
   ColorModeProvider,
+  DarkMode,
+  LightMode,
   useColorMode,
 };
