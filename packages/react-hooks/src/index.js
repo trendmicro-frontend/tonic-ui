11c28f83117d5f89e3e815319031a695c7ac6e49
Cheton Wu <cheton@gmail.com>
2021-12-25 22:29:39 +0800

    feat(react-hooks): add `useConst` Hook (#436)
    * feat(react-hooks): add `useConst` Hook
    
    * chore: rename hook to Hook

--- packages/react-hooks/src/index.js
+++ packages/react-hooks/src/index.js
@@ -1,3 +1,4 @@
+export useConst from './useConst';
 export useEffectOnce from './useEffectOnce';
 export useHydrated from './useHydrated';
 export usePrevious from './usePrevious';
---
+++ packages/react-hooks/src/useConst.js
@@ -0,0 +1,17 @@
+import { useRef } from 'react';
+
+/**
+ * `useConst` is a custom Hook that creates a constant value over the lifecycle of a component.
+ * @param {any} value The value to create a constant for. If value is a function, it will be called to get the constant value.
+ */
+const useConst = (init) => {
+  const ref = useRef(null);
+
+  if (ref.current === null) {
+    ref.current = (typeof init === 'function') ? init() : init;
+  }
+
+  return ref.current;
+};
+
+export default useConst;
