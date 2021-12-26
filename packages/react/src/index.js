7255e9d633ffa58ee00c33f781b39b338031af0a
cheton <cheton@gmail.com>
2021-12-26 13:24:30 +0800

    chore: rework package exports

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -1,212 +1,51 @@
+// components
+export * from './alert';
+export * from './badge';
+export * from './box';
+export * from './button';
+export * from './checkbox';
+export * from './color-mode';
+export * from './color-style';
+export * from './css-baseline';
+export * from './divider';
+export * from './drawer';
+export * from './flat-button';
+export * from './flex';
+export * from './grid';
+export * from './icon';
+export * from './image';
+export * from './input';
+export * from './link';
+export * from './menu';
+export * from './modal';
+export * from './pagination';
+export * from './popover';
+export * from './popper';
+export * from './portal';
+export * from './presence';
+export * from './radio';
+export * from './search-input';
+export * from './scrollbar';
+export * from './select';
+export * from './skeleton';
+export * from './space';
+export * from './spinner';
+export * from './stack';
+export * from './switch';
+export * from './table';
+export * from './tabs';
+export * from './tag';
+export * from './text';
+export * from './textarea';
+export * from './theme';
+export * from './toast';
+export * from './tooltip';
+export * from './transitions';
+export * from './visually-hidden';
+
+// hooks
 export useDisclosure from './deprecated/useDisclosure'; // deprecated
+
+// settings
 export colorStyle from './shared/color-style';
 export theme from './shared/theme';
-
-export {
-  Alert,
-  AlertCloseButton,
-  AlertIcon,
-  AlertMessage,
-} from './alert';
-export {
-  Badge,
-} from './badge';
-export {
-  Box,
-  ControlBox,
-  PseudoBox, // deprecated
-} from './box';
-export {
-  Button,
-  ButtonBase,
-  ButtonGroup,
-  useButtonGroup,
-} from './button';
-export {
-  Checkbox,
-  CheckboxGroup,
-  useCheckboxGroup,
-} from './checkbox';
-export {
-  ColorModeProvider,
-  DarkMode,
-  LightMode,
-  useColorMode,
-} from './color-mode';
-export {
-  ColorStyleProvider,
-  useColorStyle,
-} from './color-style';
-export {
-  CSSBaseline,
-} from './css-baseline';
-export {
-  Divider,
-} from './divider';
-export {
-  Drawer,
-  DrawerBody,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-} from './drawer';
-export {
-  FlatButton,
-} from './flat-button';
-export {
-  Flex,
-} from './flex';
-export {
-  Grid,
-} from './grid';
-export {
-  Icon,
-  SVGIcon,
-} from './icon';
-export {
-  Image,
-} from './image';
-export {
-  Input,
-  InputBase,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupAppend,
-  InputGroupPrepend,
-  useInputGroup,
-} from './input';
-export {
-  Link,
-  LinkButton,
-} from './link';
-export {
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuGroup,
-  MenuItem,
-  MenuList,
-  MenuToggle,
-  MenuToggleIndicator,
-} from './menu';
-export {
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-} from './modal';
-export {
-  Pagination,
-  usePagination,
-} from './pagination';
-export {
-  Popover,
-  PopoverBody,
-  PopoverContent,
-  PopoverFooter,
-  PopoverHeader,
-  PopoverTrigger,
-} from './popover';
-export {
-  Popper,
-  PopperArrow,
-} from './popper';
-export {
-  Portal,
-} from './portal';
-export {
-  Presence,
-  usePresence,
-} from './presence';
-export {
-  Radio,
-  RadioGroup,
-  useRadioGroup,
-} from './radio';
-export {
-  SearchInput,
-} from './search-input';
-export {
-  Scrollbar,
-} from './scrollbar';
-export {
-  Select,
-  Option,
-  OptionGroup,
-} from './select';
-export {
-  Skeleton,
-} from './skeleton';
-export {
-  Space,
-} from './space';
-export {
-  Spinner,
-} from './spinner';
-export {
-  Stack,
-} from './stack';
-export {
-  Switch,
-  ToggleSwitch, // deprecated
-} from './switch';
-export {
-  Table,
-  TableBody,
-  TableCell,
-  TableHeader,
-  TableHeaderCell,
-  TableHeaderRow,
-  TableRow,
-} from './table';
-export {
-  Tab,
-  Tabs,
-  TabList,
-  TabPanel,
-  TabPanels,
-} from './tabs';
-export {
-  Tag,
-  TagCloseButton,
-} from './tag';
-export {
-  Text,
-  TextLabel,
-} from './text';
-export {
-  Textarea,
-} from './textarea';
-export {
-  ThemeProvider,
-  useTheme,
-  withTheme, // deprecated
-} from './theme';
-export {
-  Toast,
-  ToastCloseButton,
-  ToastContainer,
-  ToastController,
-  ToastIcon,
-  ToastMessage,
-  ToastProvider,
-  ToastTransition,
-  useToast,
-} from './toast';
-export {
-  Tooltip,
-} from './tooltip';
-export {
-  Collapse,
-  Fade,
-  Grow,
-  Scale,
-  Slide,
-  Zoom,
-} from './transitions';
-export {
-  VisuallyHidden,
-} from './visually-hidden';
