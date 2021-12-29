265e8a834c0ae6596f1b8714efdec7f69868e0fc
cheton <cheton@gmail.com>
2021-12-29 22:48:16 +0800

    fix: fix incorrectly spelled path

--- packages/react-docs/update-versions.sh
+++ packages/react-docs/update-versions.sh
@@ -2,7 +2,7 @@
 
 TONIC_UI_VARS=$(env | grep "TONIC_UI_"  | cut -d "=" -f 1)
 
-cp -f pages/templates/versions.mdx.template pages/getting-started/versions.mdx
+cp -f templates/versions.mdx.template pages/getting-started/versions.mdx
 
 for var in $TONIC_UI_VARS; do
   sed -i'.bak' -e "s|__${var}__|${!var}|g" pages/getting-started/versions.mdx
