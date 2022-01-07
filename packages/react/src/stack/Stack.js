7e55a548e368ae2e3a2cbc3c8d9f4aa7421a09f3
cheton <cheton@gmail.com>
2022-01-07 21:38:06 +0800

    feat: remove "cloneElement" from the `Stack` component

--- packages/react/src/stack/Stack.js
+++ packages/react/src/stack/Stack.js
@@ -1,55 +1,51 @@
 import React from 'react';
 import { Box } from '../box';
+import { useStackStyle } from './styles';
+
+const defaultDirection = 'column';
+
+const uniqueId = (() => {
+  let id = 0;
+  return () => {
+    id += 1;
+    return String(id);
+  };
+})();
 
 const Stack = ({
   children,
-  direction = 'column',
+  direction,
   flexDirection,
-  spacing = 0,
   shouldWrapChildren,
+  spacing = 0,
   ...rest
 }) => {
   const validChildrenArray = React.Children
     .toArray(children)
     .filter(c => React.isValidElement(c));
 
+  direction = (flexDirection ?? direction) ?? defaultDirection;
+
+  const styleProps = useStackStyle({ direction, spacing });
+
   return (
     <Box
-      display="flex"
-      flexDirection={flexDirection ?? direction}
+      {...styleProps}
       {...rest}
     >
       {validChildrenArray.map((child, index) => {
-        const isLastChild = ((index + 1) === validChildrenArray.length);
-        const id = index;
-        const spacingProps = {
-          'column': {
-            'mb': isLastChild ? null : spacing,
-          },
-          'column-reverse': {
-            'mt': isLastChild ? null : spacing,
-          },
-          'row': {
-            'mr': isLastChild ? null : spacing,
-          },
-          'row-reverse': {
-            'ml': isLastChild ? null : spacing,
-          },
-        }[flexDirection ?? direction];
-
         if (shouldWrapChildren) {
           return (
             <Box
-              key={`stack-box-wrapper-${id}`}
-              display="inline-block"
-              {...spacingProps}
+              key={uniqueId()}
+              display="inline-flex"
             >
               {child}
             </Box>
           );
         }
 
-        return React.cloneElement(child, spacingProps);
+        return child;
       })}
     </Box>
   );
