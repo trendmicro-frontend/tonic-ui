4fbc900d1df2b549b8da62dd471a539b7331b10c
cheton <cheton@gmail.com>
2022-01-21 18:33:27 +0800

    chore: work in progress

--- packages/react-docs/components/GlobalStyles.jsx
+++ packages/react-docs/components/GlobalStyles.jsx
@@ -1,11 +1,21 @@
 import { Global, css } from '@emotion/react';
 import {
+  useColorMode,
   useTheme,
 } from '@tonic-ui/react';
 import React from 'react';
 
 const GlobalStyles = () => {
+  const [colorMode] = useColorMode();
   const theme = useTheme();
+  const backgroundColor = {
+    light: 'white',
+    dark: 'gray:100',
+  }[colorMode];
+  const color = {
+    light: 'black:primary',
+    dark: 'white:primary',
+  }[colorMode];
 
   /**
    * The following custom properties are defined in "pages/_document.js"
@@ -18,14 +28,14 @@ const GlobalStyles = () => {
     <Global
       styles={css`
         :root {
-          color-scheme: var(--root-color-scheme);
+          color-scheme: ${colorMode};
         }
         :focus:not(.focus-visible) {
           outline: none;
         }
         body {
-          background-color: var(--root-background-color);
-          color: var(--root-color);
+          background-color: ${backgroundColor};
+          color: ${color};
           font-size: ${theme.fontSizes.sm};
           line-height: ${theme.lineHeights.sm};
         }
