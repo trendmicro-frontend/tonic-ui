0d9a8ad44a7a977d4d16178cf485d5c7c2c78406
cheton <cheton@gmail.com>
2022-03-02 16:19:23 +0800

    test: add test for the useEventCallback Hook

---
+++ packages/react-hooks/src/useEventCallback.test.js
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React, { useEffect, useRef, useState } from 'react';
+import useConst from './useConst';
+import useEventCallback from './useEventCallback';
+
+describe('useEventCallback', () => {
+  test('xxx', () => {
+    const App = () => {
+      const [value, setValue] = useState('');
+      // `onChange` will be memoized even if `value` changes
+      const onChange = useEventCallback((event) => {
+        const nextValue = event.target.value;
+        setValue(nextValue);
+      }, [value]);
+      const memoizedEventCallback = useConst(() => onChange);
+
+      useEffect(() => {
+        expect(onChange).toEqual(memoizedEventCallback);
+      }, [value]);
+
+      return (
+        <input value={value} onChange={onChange} placeholder="Enter your text" />
+      );
+    };
+
+    const result = render(<App />);
+    const input = result.container.querySelector('input');
+    userEvent.type(input, 'Hello');
+  });
+});
+
