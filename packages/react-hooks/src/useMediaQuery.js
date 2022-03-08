bd7cd54ae7cb3857ba06efaae63d1ee2670a42d8
cheton <cheton@gmail.com>
2022-03-08 18:25:28 +0800

    feat: add useMediaQuery Hook

---
+++ packages/react-hooks/src/useMediaQuery.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+const useMediaQuery = (query) => {
+  const [match, setMatch] = useState(null);
+
+  useEffect(() => {
+    const mediaQuery = window?.matchMedia(query);
+    if (!mediaQuery) {
+      return;
+    }
+    const handler = () => {
+      setMatch(!!(mediaQuery.matches));
+    };
+    handler();
+    mediaQuery.addEventListener('change', handler);
+    return () => {
+      mediaQuery.removeEventListener('change', handler);
+    };
+  }, [query]);
+
+  return match;
+};
+
+export default useMediaQuery;
