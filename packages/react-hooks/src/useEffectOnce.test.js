7bfba42d3ffa4330d347451d05e217eb9c04a3e2
cheton <cheton@gmail.com>
2022-03-09 13:58:54 +0800

    test: add tests for the useEffectOnce Hook

---
+++ packages/react-hooks/src/useEffectOnce.test.js
@@ -0,0 +1,28 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import { useState } from 'react';
+import useEffectOnce from './useEffectOnce';
+
+const mockEffectCleanup = jest.fn();
+const mockEffectCallback = jest.fn().mockReturnValue(mockEffectCleanup);
+
+describe('useEffectOnce', () => {
+  beforeEach(() => jest.clearAllMocks());
+
+  it('should be defined', () => {
+    expect(useEffectOnce).toBeDefined();
+  });
+
+  it('should run provided effect only once', () => {
+    const { rerender } = renderHook(() => useEffectOnce(mockEffectCallback));
+    expect(mockEffectCallback).toHaveBeenCalledTimes(1);
+    rerender();
+    expect(mockEffectCallback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should run the clean-up function when unmounting', () => {
+    const { unmount } = renderHook(() => useEffectOnce(mockEffectCallback));
+    expect(mockEffectCleanup).not.toHaveBeenCalled();
+    unmount();
+    expect(mockEffectCleanup).toHaveBeenCalledTimes(1);
+  });
+});
