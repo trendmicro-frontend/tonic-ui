41aa7d449026888a2e6fd4b8f87875de7062979f
cheton <cheton@gmail.com>
2022-03-09 11:28:28 +0800

    chore: fix eslint errors

--- packages/react-hooks/src/useMediaQuery.js
+++ packages/react-hooks/src/useMediaQuery.js
@@ -1,20 +1,24 @@
 import { useEffect, useState } from 'react';
 
+const noop = () => {};
+
 const useMediaQuery = (query) => {
   const [match, setMatch] = useState(null);
 
   useEffect(() => {
     const mediaQuery = window?.matchMedia(query);
-    if (mediaQuery) {
-      const handler = () => {
-        setMatch(!!(mediaQuery.matches));
-      };
-      handler();
-      mediaQuery.addEventListener('change', handler);
-      return () => {
-        mediaQuery.removeEventListener('change', handler);
-      };
+    if (!mediaQuery) {
+      return noop;
     }
+
+    const handler = () => {
+      setMatch(!!(mediaQuery.matches));
+    };
+    handler();
+    mediaQuery.addEventListener('change', handler);
+    return () => {
+      mediaQuery.removeEventListener('change', handler);
+    };
   }, [query]);
 
   return match;
