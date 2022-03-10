b65d54bf28445db544464bd47a9499ca445be4a7
tina_c_lin <tina_c_lin@trendmicro.com>
2022-03-10 16:24:03 +0800

    chore: work in progress

--- packages/react/src/tag/Tag.js
+++ packages/react/src/tag/Tag.js
@@ -43,7 +43,6 @@ const Tag = forwardRef((
     isClosable = isClosable || isCloseButtonVisible; // TODO: remove this line after deprecation
   }
 
-  const canFocus = isClosable;
   const tagStyleProps = useTagStyle({
     color: variantColor, // TODO: remove this line after deprecation
     size,
