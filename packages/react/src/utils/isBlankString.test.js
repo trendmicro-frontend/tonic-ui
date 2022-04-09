9e4c8f8ef20e2d6ddf3ad33a6651e8fae70a07c7
cheton <cheton@gmail.com>
2022-04-09 14:12:19 +0800

    feat: add isBlankString to check whether the value is a blank string

---
+++ packages/react/src/utils/isBlankString.test.js
@@ -0,0 +1,21 @@
+import isBlankString from './isBlankString';
+
+describe('Check whether the value is a blank string', () => {
+  it('should return true', () => {
+    expect(isBlankString('')).toBe(true);
+    expect(isBlankString('  ')).toBe(true);
+    expect(isBlankString('\t')).toBe(true);
+    expect(isBlankString('\r\n ')).toBe(true);
+    expect(isBlankString(' \t\r\n ')).toBe(true);
+  });
+
+  it('should return false', () => {
+    expect(isBlankString([])).toBe(false);
+    expect(isBlankString({})).toBe(false);
+    expect(isBlankString(0)).toBe(false);
+    expect(isBlankString(function(){})).toBe(false);
+    expect(isBlankString(null)).toBe(false);
+    expect(isBlankString(undefined)).toBe(false);
+    expect(isBlankString('string')).toBe(false);
+  });
+});
