71e34f0dce218ebdd1e149e15fb0a66f0a65a368
cheton <cheton@gmail.com>
2022-04-09 14:13:31 +0800

    feat: add tests for isNullOrUndefined

---
+++ packages/react/src/utils/isNullOrUndefined.test.js
@@ -0,0 +1,21 @@
+import isNullOrUndefined from './isNullOrUndefined';
+
+describe('Check whether a value is null or undefined', () => {
+  it('should return true', () => {
+    expect(isNullOrUndefined()).toBe(true);
+    expect(isNullOrUndefined(null)).toBe(true);
+    expect(isNullOrUndefined(undefined)).toBe(true);
+  });
+
+  it('should return false', () => {
+    expect(isNullOrUndefined('null')).toBe(false);
+    expect(isNullOrUndefined('undefined')).toBe(false);
+    expect(isNullOrUndefined([])).toBe(false);
+    expect(isNullOrUndefined({})).toBe(false);
+    expect(isNullOrUndefined(0)).toBe(false);
+    expect(isNullOrUndefined(function(){})).toBe(false);
+    expect(isNullOrUndefined('')).toBe(false);
+    expect(isNullOrUndefined('    ')).toBe(false);
+    expect(isNullOrUndefined('\r\t\n ')).toBe(false);
+  });
+});
