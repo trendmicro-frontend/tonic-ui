2beb764b3ea91a56d52386b7cd3dd333d6808ab4
cheton <cheton@gmail.com>
2022-04-14 17:43:41 +0800

    ci: remove conflicted "react-hooks" from "docs/.eslintrc.js"

--- packages/docs/.eslintrc.js
+++ packages/docs/.eslintrc.js
@@ -11,11 +11,8 @@ module.exports = {
   },
   plugins: [
     '@babel',
-    'react-hooks',
   ],
   rules: {
     'react/prop-types': 0,
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies
   },
 };
