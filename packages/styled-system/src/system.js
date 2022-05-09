5769c4cf522cceb20b0057b9d3836028a90a8887
cheton <cheton@gmail.com>
2022-05-09 22:23:50 +0800

    feat(styled-system): add scrolling style props

--- packages/styled-system/src/system.js
+++ packages/styled-system/src/system.js
@@ -16,6 +16,7 @@ import margin from './config/margin';
 import outline from './config/outline';
 import padding from './config/padding';
 import position from './config/position';
+import scroll from './config/scroll';
 import shape from './config/shape';
 import text from './config/text';
 import transform from './config/transform';
@@ -40,6 +41,7 @@ const system = compose(
   outline,
   padding,
   position,
+  scroll,
   shape,
   text,
   transform,
