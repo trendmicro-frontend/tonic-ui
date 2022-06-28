95c3e59f7243d98a5d9b9f8cb07e6030f1e47ec5
cheton <cheton@gmail.com>
2022-06-28 17:25:37 +0800

    feat: rework setRef and useForkRef utils

--- packages/react/src/utils/useForkRef.js
+++ packages/react/src/utils/useForkRef.js
@@ -1,22 +1,15 @@
 import * as React from 'react';
-import setRef from './setRef';
+import { mergeRefs } from './refs';
 
-const useForkRef = (refA, refB) => {
+const useForkRef = (...refs) => {
   /**
    * This will create a new function if the ref props change and are defined.
    * This means React will call the old forkRef with `null` and the new forkRef
    * with the ref. Cleanup naturally emerges from this behavior.
    */
   return React.useMemo(() => {
-    if ((refA === null || refA === undefined) && (refB === null || refB === undefined)) {
-      return null;
-    }
-
-    return (refValue) => {
-      setRef(refA, refValue);
-      setRef(refB, refValue);
-    };
-  }, [refA, refB]);
+    return mergeRefs(...refs);
+  }, [...refs]); // eslint-disable-line react-hooks/exhaustive-deps
 };
 
 export default useForkRef;
