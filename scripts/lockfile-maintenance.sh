01819bd750f8ea87c992e0e2b31f39321e3ef9f8
Cheton Wu <cheton@gmail.com>
2022-10-25 11:25:55 +0800

    feat: weekly lockfile maintenance (#633)
    * feat: weekly lockfile maintenance
    
    * chore: update the lockfile prior to install

---
+++ scripts/lockfile-maintenance.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+MAIN_BRANCH=master
+PR_BRANCH=feat/lockfile-maintenance-ci-job-$(date +"%m-%d-%Y")
+OWNER=trendmicro-frontend
+REPO=tonic-ui
+BODY='{"head":''"'${PR_BRANCH}'"'',"base":"master","title":"ci: weekly lockfile maintenance"}'
+
+git checkout $MAIN_BRANCH
+git checkout -b $PR_BRANCH
+yarn up
+git add yarn.lock
+git commit -m "ci: weekly lockfile maintenance"
+git push --set-upstream origin $PR_BRANCH
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GH_TOKEN}" \
+  -d "$BODY" \
+  https://api.github.com/repos/$OWNER/$REPO/pulls
