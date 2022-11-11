afd1faa0f423b278c973ad3a89a73d1a38565a1b
cheton <cheton@gmail.com>
2022-11-11 19:05:41 +0800

    test: add skeleton.test.js

---
+++ packages/react/src/skeleton/__tests__/skeleton.test.js
@@ -0,0 +1,69 @@
+import { getByTestId } from '@testing-library/dom';
+import { render } from '@tonic-ui/react/test-utils/render';
+import { DarkMode, LightMode, Skeleton } from '@tonic-ui/react/src';
+import * as React from 'react';
+
+describe('Skeleton', () => {
+  it('should render correctly', () => {
+    { // Dark mode
+      const { container } = render(
+        <DarkMode>
+          <Skeleton data-testid="text" variant="text" width={120} />
+          <Skeleton data-testid="rect" variant="rect" width={120} height={40} />
+          <Skeleton data-testid="circle" variant="circle" width="10x" height="10x" />
+        </DarkMode>
+      );
+
+      const expectedBackgroundColor = 'rgba(255, 255, 255, 0.08)';
+
+      // text
+      expect(getByTestId(container, 'text')).toHaveStyle({
+        'background-color': expectedBackgroundColor,
+        height: '.75rem',
+        width: '120px',
+      });
+      // rect
+      expect(getByTestId(container, 'rect')).toHaveStyle({
+        'background-color': expectedBackgroundColor,
+        height: '40px',
+        width: '120px',
+      });
+      // circle
+      expect(getByTestId(container, 'circle')).toHaveStyle({
+        'background-color': expectedBackgroundColor,
+        height: '2.5rem',
+        width: '2.5rem',
+      });
+    }
+
+    { // Light mode
+      const { container } = render(
+        <LightMode>
+          <Skeleton data-testid="text" variant="text" width={120} />
+          <Skeleton data-testid="rect" variant="rect" width={120} height={40} />
+          <Skeleton data-testid="circle" variant="circle" width="10x" height="10x" />
+        </LightMode>
+      );
+
+      const expectedBackgroundColor = 'rgba(0, 0, 0, 0.08)';
+      // text
+      expect(getByTestId(container, 'text')).toHaveStyle({
+        'background-color': expectedBackgroundColor,
+        height: '.75rem',
+        width: '120px',
+      });
+      // rect
+      expect(getByTestId(container, 'rect')).toHaveStyle({
+        'background-color': expectedBackgroundColor,
+        height: '40px',
+        width: '120px',
+      });
+      // circle
+      expect(getByTestId(container, 'circle')).toHaveStyle({
+        'background-color': expectedBackgroundColor,
+        height: '2.5rem',
+        width: '2.5rem',
+      });
+    }
+  });
+});
