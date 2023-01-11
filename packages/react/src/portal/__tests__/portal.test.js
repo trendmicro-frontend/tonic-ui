781565730f801018e619845d1843dc022f3d165d
cheton <cheton@gmail.com>
2023-01-11 15:10:16 +0800

    test: add tests for the Portal component

---
+++ packages/react/src/portal/__tests__/portal.test.js
@@ -0,0 +1,85 @@
+import { screen } from '@testing-library/react';
+import { render } from '@tonic-ui/react/test-utils/render';
+import { Portal } from '@tonic-ui/react/src';
+import * as React from 'react';
+
+describe('Portal', () => {
+  it('should render correctly', () => {
+    const { baseElement } = render(
+      <Portal>
+        This is a portal
+        <Portal>This is another portal</Portal>
+      </Portal>
+    );
+
+    expect(baseElement).toMatchInlineSnapshot(`
+      <body>
+        <div />
+        <div
+          class="tonic-ui-portal"
+        >
+          This is a portal
+        </div>
+        <div
+          class="tonic-ui-portal"
+        >
+          This is another portal
+        </div>
+      </body>
+    `);
+  });
+
+  it('should render nested portal', () => {
+    const { baseElement } = render(
+      <Portal>
+        This is a portal
+        <Portal appendToParentPortal={true}>This is a nested portal</Portal>
+      </Portal>
+    );
+
+    const portals = Array.from(
+      baseElement.querySelectorAll(Portal.selector)
+    );
+
+    const [parentPortal, childPortal] = portals;
+    expect(parentPortal).toContainElement(childPortal);
+  });
+
+  it('should render in a different node', () => {
+    render(
+      <div data-testid="parent">
+        <div data-testid="child-1">Child 1</div>
+        <Portal>
+          <div data-testid="child-2">Child 2</div>
+        </Portal>
+      </div>
+    );
+
+    const parent = screen.getByTestId('parent');
+    const child1 = screen.getByTestId('child-1');
+    const child2 = screen.getByTestId('child-2');
+
+    expect(parent).toContainElement(child1);
+    expect(parent).not.toContainElement(child2);
+  });
+
+  it('should render into a custom container', () => {
+    const TestComponent = () => {
+      const ref = React.useRef(null);
+      return (
+        <>
+          <div data-testid="container" ref={ref} />
+          <Portal containerRef={ref}>
+            <div data-testid="content">Hello world</div>
+          </Portal>
+        </>
+      );
+    };
+
+    render(<TestComponent />);
+
+    const content = screen.getByTestId('content');
+    const container = screen.getByTestId('container');
+    expect(container).toContainElement(content);
+  });
+});
