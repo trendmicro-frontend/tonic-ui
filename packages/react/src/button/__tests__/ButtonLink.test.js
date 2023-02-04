e0b474fcc944a17018a93ef05243a94225716b0d
cheton <cheton@gmail.com>
2023-02-04 21:27:32 +0800

    test: update tests

--- packages/react/src/button/__tests__/ButtonLink.test.js
+++ packages/react/src/button/__tests__/ButtonLink.test.js
@@ -1,24 +1,25 @@
 import { screen } from '@testing-library/react';
 import { render } from '@tonic-ui/react/test-utils/render';
 import { testA11y } from '@tonic-ui/react/test-utils/accessibility';
-import { Link } from '@tonic-ui/react/src';
+import { ButtonLink } from '@tonic-ui/react/src';
 import React from 'react';
 
-describe('Link', () => {
+describe('ButtonLink', () => {
   it('should render correctly', async () => {
     const { container } = render(
-      <Link
+      <ButtonLink
         href="https://github.com/trendmicro-frontend/tonic-ui"
         target="_blank"
+        textDecoration="none"
       >
         GitHub
-      </Link>
+      </ButtonLink>
     );
     await testA11y(container);
   });
 
   it('verifies the link has the correct href value', () => {
-    render(<Link href="/logout">Log out</Link>);
+    render(<ButtonLink href="/logout">Log out</ButtonLink>);
     const link = screen.getByRole('link', { name: /log out/i });
     expect(link.href).toBe('http://localhost/logout');
   });
