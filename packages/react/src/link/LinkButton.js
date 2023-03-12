3252b5f175572dcd0ee4d60b7ce5371f3be5ee3f
cheton <cheton@gmail.com>
2023-03-12 17:59:52 +0800

    feat: remove the `lineHeight` prop from the `Link` component

--- packages/react/src/link/LinkButton.js
+++ packages/react/src/link/LinkButton.js
@@ -1,12 +1,16 @@
 import React, { forwardRef } from 'react';
 import { ButtonBase } from '../button';
 import Link from './Link';
+import { useLinkButtonStyle } from './styles';
 
 const LinkButton = forwardRef((props, ref) => {
+  const styleProps = useLinkButtonStyle();
+
   return (
     <Link
       as={ButtonBase}
       ref={ref}
+      {...styleProps}
       {...props}
     />
   );
