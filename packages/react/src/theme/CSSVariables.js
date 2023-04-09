51cb35e23148b6eb36220f47f7b5d438bc57f1d6
cheton <cheton@gmail.com>
2023-04-09 20:02:04 +0800

    feat: root elements for CSS variables

--- packages/react/src/theme/CSSVariables.js
+++ packages/react/src/theme/CSSVariables.js
@@ -3,7 +3,7 @@ import { ensureArray } from 'ensure-type';
 import React, { useCallback } from 'react';
 
 const CSSVariables = ({
-  root = [':host', ':root'],
+  root = [':root', ':host'],
 }) => {
   const styles = useCallback((theme) => {
     const selector = ensureArray(root).join(',');
