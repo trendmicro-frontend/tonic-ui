c59bfc41de34601c757d96dd4046554182d6097a
cheton <cheton@gmail.com>
2023-04-16 10:38:36 +0800

    chore: update TableBody component

--- packages/react/src/table/TableBody.js
+++ packages/react/src/table/TableBody.js
@@ -1,10 +1,14 @@
 import React, { forwardRef } from 'react';
 import { Box } from '../box';
+import { useTableBodyStyle } from './styles';
 
 const TableBody = forwardRef((props, ref) => {
+  const styleProps = useTableBodyStyle();
+
   return (
     <Box
       ref={ref}
+      {...styleProps}
       {...props}
     />
   );
