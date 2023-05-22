ccdd225650b0282e0ff0edb87d12a01112fb7ef6
ci-job <no-reply@github.com>
2023-05-22 09:57:27 +0000

    [CI] Deploy '1.20.2' to 'gh-pages' (tag='@tonic-ui/react@1.20.2')

---
+++ react/1.20.2/_next/static/chunks/pages/components/table/usage-d2e948682701484a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9390],{99156:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/table/usage",function(){return u(46326)}])}},function(n){n.O(0,[6326,9774,2888,179],function(){return n(n.s=99156)}),_N_E=n.O()}]);
