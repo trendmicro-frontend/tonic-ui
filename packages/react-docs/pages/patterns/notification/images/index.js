b8d9e144921e0e71ff776ce043af8ee9ae1388b8
cheton <cheton@gmail.com>
2023-05-30 16:04:11 +0800

    docs: add notification center example

---
+++ packages/react-docs/pages/patterns/notification/images/index.js
@@ -0,0 +1,8 @@
+export alert from './icon-alert-144x96.svg';
+export notificationError from './icon-notification-error.svg';
+export notificationHighlight from './icon-notification-highlight.svg';
+export notificationInfo from './icon-notification-info.svg';
+export notificationProgress from './icon-notification-progress.svg';
+export notificationSuccess from './icon-notification-success.svg';
+export notificationWarning from './icon-notification-warning.svg';
+
