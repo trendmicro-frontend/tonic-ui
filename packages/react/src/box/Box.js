6e98ad974396037d9e64bfef0779c2dfee3be767
cheton <cheton@gmail.com>
2023-06-05 18:02:28 +0800

    feat: allow the usage of pseudo style props through the `sx` prop

--- packages/react/src/box/Box.js
+++ packages/react/src/box/Box.js
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import isPropValid from '@emotion/is-prop-valid';
 import { pseudoClassSelector, pseudoElementSelector, sx, system } from '@tonic-ui/styled-system';
-import { ensureArray } from 'ensure-type';
+import { ensureArray, ensurePlainObject } from 'ensure-type';
 
 const shouldForwardProp = (() => {
   const stylePropMap = ensureArray(system.propNames)
@@ -19,53 +19,16 @@ const shouldForwardProp = (() => {
   return prop => isPropValid(prop) && !omittedStylePropMap[prop];
 })();
 
+const transformCSSPseudoSelectors = (props) => {
+  const entries = Object.entries(ensurePlainObject(props))
+    .filter(([name, value]) => Object.hasOwn(pseudoClassSelector, name) || Object.hasOwn(pseudoElementSelector, name));
+
+  return sx(Object.fromEntries(entries));
+};
+
 const transformCSSSuperset = (props) => {
   // The `sx` prop is a shortcut for defining custom styles that has access to the theme
   return sx(props?.sx);
-};
-
-const transformCSSPseudoSelectors = (props) => {
-  /**
-   * Pseudo-classes must be declared in a specific order, as shown below:
-   *
-   * ```
-   * :link
-   * :visited
-   * :hover
-   * :active
-   * ```
-   *
-   * Each pseudo-class corresponds to an event which can only happen later in the timeline than the one before.
-   * 1. A link is unvisited before it is visited.
-   * 2. A link is visited before it is hovered over.
-   * 3. A link is hovered over before it is in active use.
-   */
-  const orderList = [
-    '_focus',
-    '_visited',
-    '_hover',
-    '_focusHover',
-    '_active',
-    '_focusActive',
-    '_focusSelected',
-    '_focusWithin',
-    '_disabled', // `_disabled` must be placed after above pseudo-classes
-    '_enabled',
-  ];
-  const orderedEntries = Object.entries(props).sort((a, b) => {
-    const aIndex = orderList.indexOf(a[0]);
-    const bIndex = orderList.indexOf(b[0]);
-    return aIndex - bIndex;
-  });
-
-  let entries = [];
-  for (const [name, value] of orderedEntries) {
-    const selectorFunction = pseudoClassSelector[name] ?? pseudoElementSelector[name];
-    if (typeof selectorFunction === 'function') {
-      entries = entries.concat(selectorFunction(value));
-    }
-  }
-  return sx(Object.fromEntries(entries));
 };
 
 const Box = styled('div', { shouldForwardProp })(
--- packages/styled-system/src/sx.js
+++ packages/styled-system/src/sx.js
@@ -1,24 +1,75 @@
 import system from './system';
 import ensureArray from './utils/ensure-array';
 import get from './utils/get';
+import { pseudoClassSelector, pseudoElementSelector } from './pseudo';
 
-const defaultBreakpoints = [];
+const createPseudoResolver = (theme) => (styleProps) => {
+  if (styleProps === null || styleProps === undefined) {
+    return {};
+  }
 
-const responsive = styles => theme => {
+  /**
+   * Pseudo-classes must be declared in a specific order, as shown below:
+   *
+   * ```
+   * :link
+   * :visited
+   * :hover
+   * :active
+   * ```
+   *
+   * Each pseudo-class corresponds to an event which can only happen later in the timeline than the one before.
+   * 1. A link is unvisited before it is visited.
+   * 2. A link is visited before it is hovered over.
+   * 3. A link is hovered over before it is in active use.
+   */
+  const orderList = [
+    '_focus',
+    '_visited',
+    '_hover',
+    '_focusHover',
+    '_active',
+    '_focusActive',
+    '_focusSelected',
+    '_focusWithin',
+    '_disabled', // `_disabled` must be placed after above pseudo-classes
+    '_enabled',
+  ];
+  const orderedEntries = Object.entries(styleProps).sort((a, b) => {
+    const aIndex = orderList.indexOf(a[0]);
+    const bIndex = orderList.indexOf(b[0]);
+    return aIndex - bIndex;
+  });
+
+  let entries = [];
+
+  for (const [name, value] of orderedEntries) {
+    const selectorFunction = pseudoClassSelector[name] ?? pseudoElementSelector[name];
+    if (typeof selectorFunction === 'function') {
+      entries = entries.concat(selectorFunction(value));
+    } else {
+      entries = entries.concat([[name, value]]);
+    }
+  }
+
+  return Object.fromEntries(entries);
+};
+
+const createResponsiveResolver = theme => styleProps => {
   const next = {};
-  const breakpoints = ensureArray(get(theme, 'breakpoints', defaultBreakpoints));
+  const breakpoints = ensureArray(get(theme, 'breakpoints'));
   const mediaQueries = [
     null,
     ...breakpoints.map(n => `@media screen and (min-width: ${n})`),
   ];
 
-  for (const key in styles) {
-    if (!Object.prototype.hasOwnProperty.call(styles, key)) {
+  for (const key in styleProps) {
+    if (!Object.prototype.hasOwnProperty.call(styleProps, key)) {
       continue;
     }
 
-    const value = typeof styles[key] === 'function' ? styles[key](theme) : styles[key];
-    if (value == null) {
+    const value = typeof styleProps[key] === 'function' ? styleProps[key](theme) : styleProps[key];
+    if (value === null || value === undefined) {
       continue;
     }
 
@@ -44,30 +95,37 @@ const responsive = styles => theme => {
   return next;
 };
 
-const sx = (styleConfig) => (props = {}) => {
-  if (Array.isArray(styleConfig)) {
-    return styleConfig.reduce((acc, config) => ({
+const sx = (valueOrFn) => (props = {}) => {
+  if (valueOrFn === null || valueOrFn === undefined) {
+    return {};
+  }
+
+  if (Array.isArray(valueOrFn)) {
+    return valueOrFn.reduce((acc, item) => ({
       ...acc,
-      ...sx(config)(props),
+      ...sx(item)(props),
     }), {});
   }
 
   const theme = {
     ...(props.theme || props),
   };
-  const styleObject = (typeof styleConfig === 'function') ? styleConfig(theme) : styleConfig;
-  const styles = responsive(styleObject)(theme);
+  const resolvePseudo = createPseudoResolver(theme);
+  const resolveResponsive = createResponsiveResolver(theme);
+  const originalStyleProps = (typeof valueOrFn === 'function') ? valueOrFn(theme) : valueOrFn;
+  const resolvedStyleProps = resolveResponsive(resolvePseudo(originalStyleProps));
 
   let result = {};
-  for (const key in styles) {
-    if (!Object.prototype.hasOwnProperty.call(styles, key)) {
+  for (const key in resolvedStyleProps) {
+    if (!Object.hasOwn(resolvedStyleProps, key)) {
       continue;
     }
 
-    const styleValue = styles[key];
-    const value = (typeof styleValue === 'function') ? styleValue(theme) : styleValue;
+    const styleValueOrFn = resolvedStyleProps[key];
+    const value = (typeof styleValueOrFn === 'function') ? styleValueOrFn(theme) : styleValueOrFn;
 
     if (value && typeof value === 'object') {
+      // Make a recursive call to handle nested objects
       result[key] = sx(value)(theme);
       continue;
     }
@@ -90,23 +148,18 @@ const sx = (styleConfig) => (props = {}) => {
      *     scale: "colors"
      * }
      */
-    const styleFunction = system.config[key];
-    if (typeof styleFunction !== 'function') {
+    const _sx = system.config[key];
+    if (typeof _sx !== 'function') {
       // pass them through to the result for unknown props
       result[key] = value;
       continue;
     }
 
-    const scale = get(theme, styleFunction.scale, styleFunction.defaultScale);
-    const propsWithTheme = {
-      ...styles,
-      theme, // include "theme" in props
-    };
-    const styleResult = styleFunction(scale, value, propsWithTheme); // `sx` is a style function
+    const scale = get(theme, _sx.scale, _sx.defaultScale);
 
     result = {
       ...result,
-      ...styleResult,
+      ..._sx(scale, value, { ...resolvedStyleProps, theme }),
     };
   }
 
