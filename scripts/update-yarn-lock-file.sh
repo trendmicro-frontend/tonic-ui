ce8139363980b0484012af331071edf4c6519475
cheton <cheton@gmail.com>
2023-07-24 15:02:40 +0800

    ci: add a GitHub action to update yarn.lock file

--- scripts/update-yarn-lock-file.sh
+++ scripts/update-yarn-lock-file.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 MAIN_BRANCH=master
-PR_BRANCH=ci/regular-lockfile-maintenance-$(date +"%m-%d-%Y")
+PR_BRANCH=ci/update-yarn-lock-file-$(date +"%m-%d-%Y")
 OWNER=trendmicro-frontend
 REPO=tonic-ui
-BODY='{"head":''"'${PR_BRANCH}'"'',"base":"master","title":"ci: regular lockfile maintenance"}'
+BODY='{"head":''"'${PR_BRANCH}'"'',"base":"master","title":"ci: update yarn.lock file"}'
 
 git checkout $MAIN_BRANCH
 yarn up
 git add yarn.lock
-if [ `git status -s --untracked-files=no | wc -l` -gt 0 ]; then
+if [ `git status -s --untracked-files=no | wc -l` -eq 0 ]; then
   exit 0
 fi
 git checkout -b $PR_BRANCH
-git commit -m "ci: regular lockfile maintenance"
+git commit -m "ci: update yarn.lock file"
 git push --set-upstream origin $PR_BRANCH
 curl \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${GH_TOKEN}" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -d "$BODY" \
   https://api.github.com/repos/$OWNER/$REPO/pulls
