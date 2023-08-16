862ce6e82a04ad25c69c512c06315e431608daca
cheton <cheton@gmail.com>
2023-08-16 21:40:37 +0800

    ci: upgrade to Lerna 7 and update `devDependencies`

--- packages/react/src/skeleton/__tests__/Skeleton.test.js
+++ packages/react/src/skeleton/__tests__/Skeleton.test.js
@@ -1,4 +1,4 @@
-import { getByTestId } from '@testing-library/dom';
+import { screen } from '@testing-library/react';
 import { render } from '@tonic-ui/react/test-utils/render';
 import { DarkMode, LightMode, Skeleton } from '@tonic-ui/react/src';
 import React from 'react';
@@ -6,21 +6,21 @@ import React from 'react';
 describe('Skeleton', () => {
   it('should render correctly', () => {
     { // Dark mode
-      const { container } = render(
+      render(
         <DarkMode>
           <Skeleton
-            data-testid="text"
+            data-testid="dark:text"
             variant="text"
             width={120}
           />
           <Skeleton
-            data-testid="rectangle"
+            data-testid="dark:rectangle"
             variant="rectangle"
             width={120}
             height={40}
           />
           <Skeleton
-            data-testid="circle"
+            data-testid="dark:circle"
             variant="circle"
             width={40}
             height={40}
@@ -31,18 +31,18 @@ describe('Skeleton', () => {
       const expectedBackgroundColor = 'rgba(255, 255, 255, 0.08)';
 
       // text
-      expect(getByTestId(container, 'text')).toHaveStyle({
+      expect(screen.getByTestId('dark:text')).toHaveStyle({
         'background-color': expectedBackgroundColor,
         width: '120px',
       });
       // rectangle
-      expect(getByTestId(container, 'rectangle')).toHaveStyle({
+      expect(screen.getByTestId('dark:rectangle')).toHaveStyle({
         'background-color': expectedBackgroundColor,
         height: '40px',
         width: '120px',
       });
       // circle
-      expect(getByTestId(container, 'circle')).toHaveStyle({
+      expect(screen.getByTestId('dark:circle')).toHaveStyle({
         'background-color': expectedBackgroundColor,
         height: '40px',
         width: '40px',
@@ -50,21 +50,21 @@ describe('Skeleton', () => {
     }
 
     { // Light mode
-      const { container } = render(
+      render(
         <LightMode>
           <Skeleton
-            data-testid="text"
+            data-testid="light:text"
             variant="text"
             width={120}
           />
           <Skeleton
-            data-testid="rectangle"
+            data-testid="light:rectangle"
             variant="rectangle"
             width={120}
             height={40}
           />
           <Skeleton
-            data-testid="circle"
+            data-testid="light:circle"
             variant="circle"
             width={40}
             height={40}
@@ -73,19 +73,20 @@ describe('Skeleton', () => {
       );
 
       const expectedBackgroundColor = 'rgba(0, 0, 0, 0.08)';
+
       // text
-      expect(getByTestId(container, 'text')).toHaveStyle({
+      expect(screen.getByTestId('light:text')).toHaveStyle({
         'background-color': expectedBackgroundColor,
         width: '120px',
       });
       // rectangle
-      expect(getByTestId(container, 'rectangle')).toHaveStyle({
+      expect(screen.getByTestId('light:rectangle')).toHaveStyle({
         'background-color': expectedBackgroundColor,
         height: '40px',
         width: '120px',
       });
       // circle
-      expect(getByTestId(container, 'circle')).toHaveStyle({
+      expect(screen.getByTestId('light:circle')).toHaveStyle({
         'background-color': expectedBackgroundColor,
         height: '40px',
         width: '40px',
