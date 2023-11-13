02190d46a5c03ff79e4e9283193e50f1f8ec8fbc
tina_c_lin <tina_c_lin@trendmicro.com>
2023-11-13 12:50:26 +0800

    fix: fix the OverflowTooltip issue in a specific environment

--- packages/react/src/tooltip/OverflowTooltip.js
+++ packages/react/src/tooltip/OverflowTooltip.js
@@ -4,6 +4,23 @@ import { Truncate } from '../truncate';
 import { useTruncateStyle } from '../truncate/styles';
 import Tooltip from './Tooltip';
 
+const hasEllipsis = (el) => {
+  const isNoWrap = el.style.whiteSpace === 'nowrap' || window.getComputedStyle(el).whiteSpace === 'nowrap';
+  if (isNoWrap) {
+    let scrollWidth = el.scrollWidth;
+    const oldWidth = el.style.width;
+    el.style.width = "max-content"; // set width to max-content to get the actual width of the element
+    const [clientRect] = el.getClientRects();
+    if (clientRect?.width > scrollWidth) {
+      scrollWidth = clientRect?.width;
+    }
+    el.style.width = oldWidth;
+    return scrollWidth > el.clientWidth;
+  }
+
+  return el.scrollHeight > el.clientHeight;
+};
+
 const OverflowTooltip = forwardRef((
   {
     children,
@@ -16,7 +33,7 @@ const OverflowTooltip = forwardRef((
   const truncateStyle = useTruncateStyle();
   const onMouseEnter = useCallback((event) => {
     const el = event.currentTarget;
-    setIsOverflown((el.scrollHeight > el.clientHeight) || (el.scrollWidth > el.clientWidth));
+    setIsOverflown(hasEllipsis(el));
   }, []);
   const onMouseLeave = useCallback((event) => {
     setIsOverflown(false);
