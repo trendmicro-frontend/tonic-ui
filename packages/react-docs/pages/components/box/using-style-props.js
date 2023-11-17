3d7ce39c9b2ccb7dc98078dfda09d00d33f9f205
cheton <cheton@gmail.com>
2023-11-17 16:36:06 +0800

    docs: update box

---
+++ packages/react-docs/pages/components/box/using-style-props.js
@@ -0,0 +1,20 @@
+import { Box } from '@tonic-ui/react';
+import React from 'react';
+
+const App = () => (
+  <Box
+    as="button"
+    backgroundColor="gray:60"
+    border="none"
+    borderRadius="sm"
+    color="white:primary"
+    cursor="pointer"
+    display="inline-flex"
+    px="3x"
+    py="2x"
+  >
+    Button
+  </Box>
+);
+
+export default App;
