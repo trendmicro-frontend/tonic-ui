d613f45b7a9b77b060d55b544724823141649e15
cheton <cheton@gmail.com>
2024-01-03 15:14:40 +0800

    ci: update Node.js version
    docs: upgrade Next.js and React versions

--- packages/react/babel.config.js
+++ packages/react/babel.config.js
@@ -1,16 +1,20 @@
+const plugins = [];
+if (process.env.NODE_ENV === 'test') {
+  // Enable async/await for jest
+  plugins.push('@babel/plugin-transform-runtime');
+}
+
 module.exports = {
   extends: '@trendmicro/babel-config',
+  plugins: [
+    ...plugins,
+    '@emotion/babel-plugin',
+  ],
   presets: [
     '@babel/preset-env',
     ['@babel/preset-react', {
-      /**
-       * Decides which runtime to use.
-       * - 'automatic' auto imports the functions that JSX transpiles to.
-       * - 'classic' does not automatic import anything.
-       */
-      'runtime': 'classic', // defaults to classic
-      //'runtime': 'automatic', // for React 16.14.0, React 17 and higher that supports the new transform
+      'runtime': 'automatic',
+      'importSource': '@emotion/react',
     }],
-    '@emotion/babel-preset-css-prop',
   ],
 };
