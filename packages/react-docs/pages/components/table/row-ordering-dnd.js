03d0be65143426e22e2891a371e91cbc204e3cd2
cheton <cheton@gmail.com>
2024-01-15 23:44:09 +0800

    docs: update row-ordering-dnd example

--- packages/react-docs/pages/components/table/row-ordering-dnd.js
+++ packages/react-docs/pages/components/table/row-ordering-dnd.js
@@ -16,11 +16,16 @@ import {
   useColorStyle,
 } from '@tonic-ui/react';
 import {
+  useMergeRefs,
+} from '@tonic-ui/react-hooks';
+import {
   dataAttr,
 } from '@tonic-ui/utils';
-import React, { useMemo, useState } from 'react';
+import { ensurePlainObject } from 'ensure-type';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import update from 'immutability-helper';
 import HandleIcon from './icons/icon-handle';
 
 /**
@@ -46,7 +51,7 @@ const assignRef = (ref, value) => {
   }
 };
 
-const DraggableRow = ({
+const DraggableItem = ({
   canDrag: canDragProp,
   canDrop: canDropProp,
   children,
@@ -54,8 +59,12 @@ const DraggableRow = ({
   onDrop: onDropProp,
   onHover: onHoverProp,
 }) => {
+  const dragRef = useRef();
+  const dragPreviewRef = useRef();
+  const dropRef = useRef();
+
   // https://react-dnd.github.io/react-dnd/docs/api/use-drag
-  const [collectedDragProps, dragRef, dragPreviewRef] = useDrag({
+  const [collectedDragProps, _dragRef, _dragPreviewRef] = useDrag({
     type: 'row',
     item: itemProp,
     canDrag: canDragProp,
@@ -64,54 +73,66 @@ const DraggableRow = ({
       // https://react-dnd.github.io/react-dnd/docs/api/drag-source-monitor
       return {
         isDragging: monitor.isDragging(),
+        monitor,
       };
     },
   })
 
   // https://react-dnd.github.io/react-dnd/docs/api/use-drop
-  const [collectedDropProps, dropRef] = useDrop({
+  const [collectedDropProps, _dropRef] = useDrop({
     accept: 'row',
-    drop: onDropProp,
-    hover: onHoverProp,
+    drop: (item, monitor) => {
+      onDropProp?.({ item, monitor, dragRef, dragPreviewRef, dropRef });
+    },
+    hover: (item, monitor) => {
+      onHoverProp?.({ item, monitor, dragRef, dragPreviewRef, dropRef });
+    },
     canDrop: canDropProp,
     collect: (monitor) => {
       // DropTargetMonitor
       // https://react-dnd.github.io/react-dnd/docs/api/drop-target-monitor
       return {
         isOver: monitor.isOver(),
+        monitor,
       };
     }
   })
 
+  const mergedDragRefs = useMergeRefs(_dragRef, dragRef);
+  const mergedDragPreviewRefs = useMergeRefs(_dragPreviewRef, dragPreviewRef);
+  const mergedDropRefs = useMergeRefs(_dropRef, dropRef);
+
   return children({
-    dragRef,
-    dragPreviewRef,
-    dropRef,
     isDragging: collectedDragProps.isDragging,
     isOver: collectedDropProps.isOver,
+    dragRef: mergedDragRefs,
+    dragPreviewRef: mergedDragPreviewRefs,
+    dropRef: mergedDropRefs,
+    dragSourceMonitor: collectedDragProps.monitor,
+    dropTargetMonitor: collectedDropProps.monitor,
   });
 };
 
 const App = () => {
   const [colorStyle] = useColorStyle();
   const initialData = useMemo(() => [
-    { id: 1, priority: 1, policy: 'Team Managers', modifiedTime: 1625875200000, modifiedBy: 'admin' },
-    { id: 2, priority: 2, policy: 'Marketing Team', modifiedTime: 1625875200000, modifiedBy: 'admin' },
-    { id: 3, priority: 3, policy: 'Sales Department', modifiedTime: 1625875200000, modifiedBy: 'admin' },
-    { id: 4, priority: 4, policy: 'Development Team', modifiedTime: 1625875200000, modifiedBy: 'admin' },
-    { id: 5, priority: 5, policy: 'IT Department', modifiedTime: 1625875200000, modifiedBy: 'admin' },
-    { id: 6, priority: null, policy: 'Server policy (Default)', modifiedTime: 1625097600000, modifiedBy: 'admin' },
-    { id: 7, priority: null, policy: 'Endpoint policy (Default)', modifiedTime: 1625097600000, modifiedBy: 'admin' },
+    { id: 1, order: 1, policy: 'Team Managers', modifiedTime: 1625875200000, modifiedBy: 'admin' },
+    { id: 2, order: 2, policy: 'Marketing Team', modifiedTime: 1625875200000, modifiedBy: 'admin' },
+    { id: 3, order: 3, policy: 'Sales Department', modifiedTime: 1625875200000, modifiedBy: 'admin' },
+    { id: 4, order: 4, policy: 'Development Team', modifiedTime: 1625875200000, modifiedBy: 'admin' },
+    { id: 5, order: 5, policy: 'IT Department', modifiedTime: 1625875200000, modifiedBy: 'admin' },
+    { id: 6, order: null, policy: 'Server policy (Default)', modifiedTime: 1625097600000, modifiedBy: 'admin' },
+    { id: 7, order: null, policy: 'Endpoint policy (Default)', modifiedTime: 1625097600000, modifiedBy: 'admin' },
   ], []);
-  const [data, setData] = useState(initialData);
+  const [items, setItems] = useState(initialData);
   const columns = useMemo(() => [
     {
-      id: 'priority',
-      header: 'Priority',
-      accessorKey: 'priority',
+      id: 'order',
+      header: 'Order',
+      accessorKey: 'order',
       cell: ({ getValue }) => {
-        const priority = getValue();
-        return priority ?? '-';
+        const order = getValue();
+        return order ?? '-';
       },
       size: 80,
     },
@@ -148,7 +169,7 @@ const App = () => {
   ], []);
 
   const table = useReactTable({
-    data,
+    data: items,
     columns,
     defaultColumn: {
       minSize: 40,
@@ -157,18 +178,32 @@ const App = () => {
     getRowId: row => row.id, //good to have guaranteed unique row ids/keys for rendering
   });
 
-  const moveRow = (draggedRowIndex, targetRowIndex) => {
-    const newData = [...data];
-    const [draggedRow] = newData.splice(draggedRowIndex, 1);
-    newData.splice(targetRowIndex, 0, draggedRow);
-    for (const [index, row] of newData.entries()) {
-      if (!row.priority) {
-        break;
-      }
-      row.priority = index + 1;
-    }
-    setData(newData);
-  };
+  const moveItem = useCallback((dragItemId, hoverItemId) => {
+    const dragItemIndex = items.findIndex(item => item.id === dragItemId);
+    const hoverItemIndex = items.findIndex(item => item.id === hoverItemId);
+    const dragItem = items[dragItemIndex];
+    let newItems = items;
+    // Swap two items
+    newItems = update(newItems, {
+      $splice: [
+        [dragItemIndex, 1],
+        [hoverItemIndex, 0, dragItem],
+      ],
+    });
+    // Update the order
+    newItems = update(newItems, {
+      $apply: items => items.map((item, index) => {
+        if (item.order > 0) {
+          return {
+            ...item,
+            order: index + 1,
+          };
+        }
+        return item;
+      }),
+    });
+    setItems(newItems);
+  }, [items]);
 
   const layout = 'flexbox'; // One of: 'flexbox', 'table'
 
@@ -185,7 +220,7 @@ const App = () => {
                   ...header.column.columnDef.style,
                 };
 
-                if (header.id === 'priority') {
+                if (header.id === 'order') {
                   styleProps.pl = '4x';
                 }
 
@@ -207,24 +242,44 @@ const App = () => {
         </TableHeader>
         <TableBody>
           {table.getRowModel().rows.map((row, index) => (
-            <DraggableRow
+            <DraggableItem
               key={row.id}
               item={() => row}
-              onDrop={(item, monitor) => {
-                const draggedRowIndex = item.index;
-                const targetRowIndex = row.index;
-                moveRow(draggedRowIndex, targetRowIndex);
+              onHover={({ item, monitor, dropRef }) => {
+                const dragItemId = monitor.getItem().original.id;
+                const dragItemOrder = monitor.getItem().original.order;
+                const hoverItemId = row.original.id;
+                const hoverItemOrder = row.original.order;
+
+                if (dragItemId === hoverItemId) {
+                  return;
+                }
+
+                const hoverBoundingRect = ensurePlainObject(dropRef.current?.getBoundingClientRect());
+                const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+                const clientOffset = monitor.getClientOffset();
+                const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+                if (dragItemOrder < hoverItemOrder && hoverClientY < hoverMiddleY) {
+                  return;
+                }
+                if (dragItemOrder > hoverItemOrder && hoverClientY > hoverMiddleY) {
+                  return;
+                }
+
+                moveItem(dragItemId, hoverItemId);
               }}
             >
               {({
                 dragRef,
                 dragPreviewRef,
                 dropRef,
+                dragSourceMonitor,
+                dropTargetMonitor,
                 isDragging,
                 isOver,
               }) => {
-                const canDrag = row.original.priority > 0;
-                const canDrop = row.original.priority > 0;
+                const canDrag = row.original.order > 0;
+                const canDrop = row.original.order > 0;
                 return (
                   <TableRow
                     ref={(node) => {
@@ -250,7 +305,7 @@ const App = () => {
                         ...cell.column.columnDef.style,
                       };
 
-                      if (cell.column.id === 'priority') {
+                      if (cell.column.id === 'order') {
                         return (
                           <TableCell
                             key={cell.id}
@@ -299,7 +354,7 @@ const App = () => {
                   </TableRow>
                 );
               }}
-            </DraggableRow>
+            </DraggableItem>
           ))}
         </TableBody>
       </Table>
