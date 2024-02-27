50dce83a3a95a2e7b8af400e6d2af4d7a4971ed7
zacharyl <zachary_legros@trendmicro.com>
2024-02-27 16:13:24 -0500

    feat: Added icon generation script

---
+++ packages/react/src/icon/utils.js
@@ -0,0 +1,33 @@
+import { keyframes } from '@emotion/react';
+
+export const cwSpin = keyframes`
+0% {
+      transform: rotate(0deg)
+  }
+  to {
+      transform: rotate(1turn)
+  }
+`;
+
+export const ccwSpin = keyframes`
+  0% {
+      transform: rotate(0deg)
+  }
+  to {
+      transform: rotate(-1turn)
+  }
+`;
+
+export const getIconStyleProps = ({ spin }) => {
+  return {
+    animation: (() => {
+      if (spin === 'ccw') {
+        return `${ccwSpin} 2s linear infinite`;
+      }
+      if (spin === 'cw' || spin === true) {
+        return `${cwSpin} 2s linear infinite`;
+      }
+      return undefined;
+    })()
+  }
+};
