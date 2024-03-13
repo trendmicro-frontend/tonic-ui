98a50dd441956e3d950106595e15108694f7834c
Cheton Wu <cheton@gmail.com>
2024-03-13 17:50:06 +0800

    feat: update SVGIcon component and remove animation styling

--- packages/react-icons/src/utils/createSVGIcon.js
+++ packages/react-icons/src/utils/createSVGIcon.js
@@ -1,27 +1,14 @@
 import React, { forwardRef, memo } from 'react';
 import SVGIcon from '../SVGIcon';
-import { useIconStyle } from '../styles';
 
 const createSVGIcon = (svgIcon, options) => {
   const displayName = (typeof options === 'string') ? options : options?.displayName;
-  const Component = forwardRef((
-    {
-      spin = false,
-      sx,
-      ...rest
-    },
-    ref,
-  ) => {
-    const styleProps = useIconStyle({ spin });
+  const Component = forwardRef((props, ref) => {
     return (
       <SVGIcon
         data-icon-name={displayName}
         ref={ref}
-        sx={[
-          styleProps,
-          ...(Array.isArray(sx) ? sx : [sx]),
-        ]}
-        {...rest}
+        {...props}
       >
         {svgIcon}
       </SVGIcon>
