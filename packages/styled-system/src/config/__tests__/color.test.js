71ce372e660ffc4cba416f7430e218cf5729abca
cheton <cheton@gmail.com>
2024-03-14 11:56:20 +0800

    test(styled-system): resolve failed tests

--- packages/styled-system/src/config/__tests__/color.test.js
+++ packages/styled-system/src/config/__tests__/color.test.js
@@ -4,13 +4,9 @@ test('returns colors styles', () => {
   const style = color({
     color: 'gold',
     colorScheme: 'dark',
-    fill: 'black',
-    stroke: 'black',
   });
   expect(style).toEqual({
     color: 'gold',
     colorScheme: 'dark',
-    fill: 'black',
-    stroke: 'black',
   });
 });
