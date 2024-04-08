51793053c03fb1b27cdd338994912a5bea9400aa
Cheton Wu <cheton@gmail.com>
2024-04-08 16:08:12 +0800

    test: removed deprecated event listeners

--- packages/react/src/color-mode/__tests__/ColorModeProvider.test.js
+++ packages/react/src/color-mode/__tests__/ColorModeProvider.test.js
@@ -10,8 +10,6 @@ Object.defineProperty(window, 'matchMedia', {
     matches: false,
     media: query,
     onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
     addEventListener: jest.fn(),
     removeEventListener: jest.fn(),
     dispatchEvent: jest.fn(),
