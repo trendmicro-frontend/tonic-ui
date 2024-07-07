e5b6e5bc54ba8068c24e7920db753e6093a3a131
Cheton Wu <447801+cheton@users.noreply.github.com>
2024-07-07 11:34:42 +0800

    ci: add `changelog-github` package to workspace for changelog generation using `Changesets` (#888)
    * ci: add `changelog-github` package to workspace for changelog generation using `Changesets`
    
    * refactor: implement PR code review suggestions
    
    * test: add test cases for the `changelog-github` package
    
    * ci: upload code coverage for `changelog-github`
    
    * test(jest): remove `--passWithNoTests` option
    
    * test(changelog-github): add export test
    
    * test(utils): add export test
    
    * test(react-icons): add export test
    
    * test(styled-system): add export test
    
    * test(theme): add export test
    
    * test(codemod): change test environment from `jsdom` to `node`
    
    * test(changelog-github): change test environment from `jsdom` to `node`
    
    * ci: update `yarn.lock` file
    
    * chore: remove `cpy-cli` from `devDependencies`
    
    * ci: bump `codecov` to 4.1.0

---
+++ packages/changelog-github/src/index.js
@@ -0,0 +1,154 @@
+/**
+ * @see https://github.com/changesets/changesets/blob/main/docs/modifying-changelog-format.md
+ */
+import { getInfo, getInfoFromPullRequest } from '@changesets/get-github-info';
+
+const validate = (options) => {
+  const repo = options?.repo;
+  if (!repo) {
+    throw new Error(
+      'Please provide a repo to this changelog generator like this:\n"changelog": ["@tonic-ui/changelog-github", { "repo": "org/repo" }]'
+    );
+  }
+};
+
+const getDependencyReleaseLine = async (changesets, dependenciesUpdated, options) => {
+  validate(options);
+
+  if (dependenciesUpdated.length === 0) {
+    return '';
+  }
+
+  const repo = options?.repo;
+
+  // Throws an exception if getInfo fails
+  const changesetCommits = await Promise.all(
+    changesets.map(async (cs) => {
+      if (cs.commit) {
+        const info = await getInfo({
+          repo: repo,
+          commit: cs.commit,
+        });
+        return info?.links?.commit;
+      }
+      return null;
+    })
+  );
+
+  const changesetLink = `- Updated dependencies [${changesetCommits.join(', ')}]`;
+  const updatedDependenciesList = dependenciesUpdated.map(
+    (dependency) => `  - ${dependency.name}@${dependency.newVersion}`
+  );
+
+  return [changesetLink, ...updatedDependenciesList].join('\n');
+};
+
+const getReleaseLine = async (changeset, type, options) => {
+  validate(options);
+
+  const repo = options?.repo;
+  let prFromSummary = 0;
+  let commitFromSummary = '';
+  const usersFromSummary = [];
+
+  const replacedChangelog = changeset.summary
+    .replace(/^\s*(?:pr|pull|pull\s+request):\s*#?(\d+)/im, (_, pr) => {
+      if (pr) {
+        prFromSummary = Number(pr) || 0;
+      }
+      return '';
+    })
+    .replace(/^\s*commit:\s*([^\s]+)/im, (_, commit) => {
+      commitFromSummary = commit;
+      return '';
+    })
+    .replace(/^\s*(?:author|user):\s*@?([^\s]+)/gim, (_, user) => {
+      usersFromSummary.push(user);
+      return '';
+    })
+    .trim();
+
+  const [firstLine, ...futureLines] = replacedChangelog
+    .split('\n')
+    .map((l) => l.trimRight());
+
+  const fetchLinks = async () => {
+    if (prFromSummary > 0) {
+      const pullRequestInfo = await getInfoFromPullRequest({
+        repo: repo,
+        pull: prFromSummary,
+      });
+      const links = pullRequestInfo.links;
+      if (commitFromSummary) {
+        const shortCommitId = commitFromSummary.slice(0, 7);
+        return {
+          ...links,
+          commit: `[\`${shortCommitId}\`](https://github.com/${repo}/commit/${commitFromSummary})`,
+        };
+      }
+      return links;
+    }
+
+    const commitToFetchFrom = commitFromSummary || changeset.commit;
+    if (commitToFetchFrom) {
+      const info = await getInfo({
+        repo: repo,
+        commit: commitToFetchFrom,
+      });
+      return info.links;
+    }
+
+    return {
+      commit: null,
+      pull: null,
+      user: null,
+    };
+  };
+
+  const links = await fetchLinks();
+
+  const users = (() => {
+    if (usersFromSummary.length > 0) {
+      return usersFromSummary.map((userFromSummary) => {
+        return `[@${userFromSummary}](https://github.com/${userFromSummary})`;
+      });
+    }
+
+    if (links.user) {
+      return [links.user];
+    }
+
+    return [];
+  })();
+
+  const byUsers = (users && users.length > 0) ? ` by ${users.join(', ')}` : '';
+
+  // Only link PR or merge commit not both
+  const linkPullOrCommit = (() => {
+    if (links.pull) {
+      return ` in ${links.pull}`;
+    }
+    if (links.commit) {
+      return ` in ${links.commit}`;
+    }
+    return '';
+  })();
+
+  const prefix = '';
+  const suffix = `${byUsers}${linkPullOrCommit}`;
+
+  let returnValue = `\n\n- ${prefix}${firstLine}${suffix}`;
+
+  if (futureLines.length > 0) {
+    returnValue += `\n${futureLines.filter(Boolean).map(l => `  ${l}`).join('\n')}`;
+  }
+
+  return returnValue;
+};
+
+const changelogFunctions = {
+  getDependencyReleaseLine,
+  getReleaseLine,
+};
+
+export default changelogFunctions;
