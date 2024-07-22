6872a93d1f1c4ec3db6b83aab4b2cfa91bc75c26
trend-alan-bie <141993185+trend-alan-bie@users.noreply.github.com>
2024-07-22 17:52:49 +0800

    docs: update usage for using as="li" for Text component (#899)

---
+++ packages/react-docs/pages/components/text/text-as-li.js
@@ -0,0 +1,18 @@
+import { Text } from '@tonic-ui/react';
+import React from 'react';
+
+const App = () => (
+  <>
+    <Text as="li" display="list-item">
+      This is an example with bullet points.
+    </Text>
+    <Text as="li" display="list-item">
+      This is an example with bullet points.
+    </Text>
+    <Text as="li" display="list-item">
+      This is an example with bullet points.
+    </Text>
+  </>
+);
+
+export default App;
