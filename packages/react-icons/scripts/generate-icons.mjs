875fc64b905689b1fa38a7608dec6f91263e57d0
cheton <cheton@gmail.com>
2024-09-01 13:17:05 +0800

    feat: refine `generate-icons` script to enhance icon name transformation and support deprecated icons

--- packages/react-icons/scripts/generate-icons.mjs
+++ packages/react-icons/scripts/generate-icons.mjs
@@ -1,7 +1,35 @@
-import * as tmicon from '@trendmicro/tmicon';
-import * as url from 'node:url';
-import * as path from 'node:path';
 import * as fs from 'node:fs';
+import * as path from 'node:path';
+import * as url from 'node:url';
+import * as tmicon from '@trendmicro/tmicon';
+
+// This function converts tmicon v4 icon names to their corrected component names in capitalized camel case,
+// ensuring alignment with the actual icon components.
+const transformV4IconName = (iconName) => {
+  return {
+    'ai-security': 'AI-security',
+    'api': 'API',
+    'api-management': 'API-management',
+    'connect-noip': 'connect-NoIP',
+    'file-pdf-o': 'file-PDF-o',
+    'file-ppt-o': 'file-PPT-o',
+    'iam': 'IAM',
+    'ie': 'IE',
+    'ioc': 'IOC',
+    'ip': 'IP',
+    'list-ol': 'list-OL',
+    'list-ul': 'list-UL',
+    'nas': 'NAS',
+    'rca': 'RCA',
+    'resize-nesw': 'resize-NESW',
+    'resize-nwse': 'resize-NWSE',
+    'security-ai': 'security-AI',
+    'tv': 'TV',
+    'url': 'URL',
+    'usb': 'USB',
+    'wmi': 'WMI',
+  }[iconName] ?? iconName;
+};
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -25,15 +53,50 @@ export default createSVGIcon(
 );
 `.trimStart();
 
-const renderIndexFile = ({ defaultImports }) => `
+const renderDeprecatedIconComponentFile = (iconComponentName, deprecatedIconComponentName) => `
+// AUTO-GENERATED FILE. DO NOT EDIT.
+import { useOnce } from '@tonic-ui/react-hooks';
+import React, { forwardRef } from 'react';
+import ${iconComponentName} from '../${iconComponentName}';
+
+const ${deprecatedIconComponentName} = forwardRef((props, ref) => {
+  useOnce(() => {
+    console.error('Warning: The \`${deprecatedIconComponentName}\` component is deprecated and will be removed in the next major release. Use the \`${iconComponentName}\` component instead.');
+  });
+
+  return (
+    <${iconComponentName} ref={ref} {...props} />
+  );
+});
+
+${deprecatedIconComponentName}.displayName = '${iconComponentName}';
+${deprecatedIconComponentName}._isDeprecated = true;
+
+export default ${deprecatedIconComponentName};
+`.trimStart();
+
+const renderIndexFile = ({ defaultImports, deprecatedDefaultImports}) => `
 // AUTO-GENERATED FILE. DO NOT EDIT.
 ${defaultImports.map(defaultImport => (`export { default as ${defaultImport} } from './${defaultImport}';`)).join('\n')}
+
+// DEPRECATED ICONS
+${deprecatedDefaultImports.map(defaultImport => (`export { default as ${defaultImport} } from './deprecated/${defaultImport}';`)).join('\n')}
 `.trimStart();
 
 const generateIcons = () => {
+  const deprecatedIcons = [];
   const icons = tmicon.icons.map(icon => {
+    const iconName = transformV4IconName(icon.name);
+    if (iconName !== icon.name) {
+      deprecatedIcons.push({
+        deprecatedName: mapKebabCaseToCapitalizedCamelCase(icon.name),
+        name: mapKebabCaseToCapitalizedCamelCase(iconName),
+        paths: icon.paths,
+      });
+    }
+
     return {
-      name: mapKebabCaseToCapitalizedCamelCase(icon.name),
+      name: mapKebabCaseToCapitalizedCamelCase(iconName),
       paths: icon.paths,
     };
   });
@@ -41,9 +104,21 @@ const generateIcons = () => {
   // generate icon component files
   for (const icon of icons) {
     const displayName = `${icon.name}Icon`;
-    const file = path.resolve(outputDirectory, `${icon.name}Icon.js`);
+    const file = path.resolve(outputDirectory, `${displayName}.js`);
     const svgCode = icon.paths.map(path => `<path d="${path}" />`).join('');
     const data = renderIconComponentFile(displayName, (icon.paths.length > 1) ? `<>${svgCode}</>` : svgCode);
+    fs.writeFileSync(file, data, { encoding: 'utf8' });
+  }
+
+  // generate deprecated icon component files
+  if (deprecatedIcons.length > 0) {
+    fs.mkdirSync(path.resolve(outputDirectory, './deprecated'));
+  }
+  for (const icon of deprecatedIcons) {
+    const displayName = `${icon.name}Icon`;
+    const deprecatedDisplayName = `${icon.deprecatedName}Icon`;
+    const file = path.resolve(outputDirectory, `./deprecated/${deprecatedDisplayName}.js`);
+    const data = renderDeprecatedIconComponentFile(displayName, deprecatedDisplayName);
     fs.writeFileSync(file, data, { encoding: 'utf8' });
   }
 
@@ -51,6 +126,7 @@ const generateIcons = () => {
   const file = path.resolve(outputDirectory, 'index.js');
   const data = renderIndexFile({
     defaultImports: icons.map(icon => `${icon.name}Icon`).sort(),
+    deprecatedDefaultImports: deprecatedIcons.map(icon => `${icon.deprecatedName}Icon`).sort(),
   });
   fs.writeFileSync(file, data, { encoding: 'utf8' });
 };
@@ -62,7 +138,20 @@ try {
   fs.mkdirSync(outputDirectory);
 }
 
-// clean up the output directory
-fs.readdirSync(outputDirectory).forEach((file) => fs.unlinkSync(path.resolve(outputDirectory, file)));
+const cleanOutputDirectory = (directoryPath) => {
+  fs.readdirSync(directoryPath).forEach((file) => {
+    const filePath = path.resolve(directoryPath, file);
+    const stats = fs.statSync(filePath);
+
+    if (stats.isDirectory()) {
+      cleanOutputDirectory(filePath);
+      fs.rmdirSync(filePath);
+    } else if (stats.isFile()) {
+      fs.unlinkSync(filePath);
+    }
+  });
+};
+
+cleanOutputDirectory(outputDirectory);
 
 generateIcons();
