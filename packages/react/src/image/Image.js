11f297b96912c9a7e2a256cd0727f398025ed2fd
cheton <cheton@gmail.com>
2024-09-15 20:58:33 +0800

    feat(react/image): apply default props

--- packages/react/src/image/Image.js
+++ packages/react/src/image/Image.js
@@ -1,14 +1,19 @@
 import React, { forwardRef } from 'react';
 import { Box } from '../box';
+import { useDefaultProps } from '../default-props';
 
-const Image = forwardRef((props, ref) => (
-  <Box
-    ref={ref}
-    as="img"
-    alt=""
-    {...props}
-  />
-));
+const Image = forwardRef((inProps, ref) => {
+  const props = useDefaultProps({ props: inProps, name: 'Image' });
+
+  return (
+    <Box
+      ref={ref}
+      as="img"
+      alt=""
+      {...props}
+    />
+  );
+});
 
 Image.displayName = 'Image';
 
