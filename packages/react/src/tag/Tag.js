7defd0b15c9e820f5a625b0c09cd4385d4b7f793
Cheton Wu <cheton@gmail.com>
2024-09-16 17:44:56 +0800

    feat(react/tag): apply default props

--- packages/react/src/tag/Tag.js
+++ packages/react/src/tag/Tag.js
@@ -2,14 +2,15 @@ import { ariaAttr, runIfFn } from '@tonic-ui/utils';
 import memoize from 'micro-memoize';
 import React, { forwardRef } from 'react';
 import { Box } from '../box';
+import { useDefaultProps } from '../default-props';
 import { useTagStyle } from './styles';
 import TagCloseButton from './TagCloseButton';
 import { TagContext } from './context';
 
 const getMemoizedState = memoize(state => ({ ...state }));
 
-const Tag = forwardRef((
-  {
+const Tag = forwardRef((inProps, ref) => {
+  const {
     children,
     disabled,
     error,
@@ -18,9 +19,7 @@ const Tag = forwardRef((
     size = 'md',
     variant = 'solid',
     ...rest
-  },
-  ref
-) => {
+  } = useDefaultProps({ props: inProps, name: 'Tag' });
   const ariaProps = {
     'aria-disabled': ariaAttr(disabled),
     'aria-invalid': ariaAttr(error),
