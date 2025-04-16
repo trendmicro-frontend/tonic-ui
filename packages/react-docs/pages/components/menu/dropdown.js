443a815641cd073c6ffdd2e508868afdec97d5ad
cheton <cheton@gmail.com>
2025-04-16 23:08:47 +0800

    docs: enhance `Dropdown` component usage

--- packages/react-docs/pages/components/menu/dropdown.js
+++ packages/react-docs/pages/components/menu/dropdown.js
@@ -1,10 +1,16 @@
 import {
+  Box,
+  Button,
+  ButtonGroup,
+  Divider,
   Flex,
-  MenuButton,
+  OverflowTooltip,
   Text,
+  TextLabel,
 } from '@tonic-ui/react';
 import React, { useState } from 'react';
 import Dropdown from '@/components/Dropdown';
+import FormGroup from '@/components/FormGroup';
 import MutedText from '@/components/MutedText';
 
 const options = [
@@ -15,55 +21,95 @@ const options = [
 
 const mapOptionValueToText = (value) => {
   return {
-    'all': 'All',
+    'all': 'All events',
     'network-events': 'Network events',
     'system-events': 'System events',
   }[value];
 };
 
 const App = () => {
+  const [width, setWidth] = useState('auto');
   const [value, setValue] = useState('all');
-  const renderOption = (option) => {
-    return mapOptionValueToText(option.value);
-  };
   const handleChange = (option) => {
     setValue(option.value);
   };
-  const renderButtonLabel = (value) => {
+  const renderValue = (value) => {
+    const labelText = 'Event status:';
+    const valueText = mapOptionValueToText(value);
     return (
-      <Flex columnGap="2x">
-        <MutedText>
-          {'Event status:'}
-        </MutedText>
-        <Text>
-          {mapOptionValueToText(value)}
-        </Text>
+      <Flex alignItems="center" columnGap="2x">
+        <OverflowTooltip label={`${labelText} ${valueText}`}>
+          {({ ref, style }) => (
+            <MutedText
+              ref={ref}
+              {...style}
+              maxWidth="100%"
+              flex="none"
+            >
+              {labelText}
+            </MutedText>
+          )}
+        </OverflowTooltip>
+        <OverflowTooltip label={valueText}>
+          {({ ref, style }) => (
+            <Text
+              ref={ref}
+              {...style}
+              flex="auto"
+            >
+              {valueText}
+            </Text>
+        )}
+        </OverflowTooltip>
       </Flex>
     );
   };
+  const renderOption = (option) => {
+    return mapOptionValueToText(option.value);
+  };
+  const changeWidthBy = (value) => () => {
+    setWidth(value);
+  };
 
   return (
-    <Dropdown
-      onChange={handleChange}
-      options={options}
-      renderOption={renderOption}
-    >
-      {({ getToggleProps }) => (
-        <MenuButton
-          {...getToggleProps()}
+    <>
+      <FormGroup>
+        <Box mb="2x">
+          <TextLabel>
+            Try changing the dropdown width to see how the overflow tooltip behaves:
+          </TextLabel>
+        </Box>
+        <ButtonGroup
           variant="secondary"
           sx={{
-            width: '100%',
-            '> :first-of-type': {
-              // Override flex item's default `minWidth: auto` to allow text truncation
-              minWidth: 0,
-            },
+            '> *:not(:first-of-type)': {
+              marginLeft: -1
+            }
           }}
         >
-          {renderButtonLabel(value)}
-        </MenuButton>
-      )}
-    </Dropdown>
+          {['auto', '100px', '150px'].map(value => (
+            <Button
+              disabled={width === 'full'}
+              key={value}
+              selected={value === width}
+              onClick={changeWidthBy(value)}
+              minWidth="15x"
+            >
+              {value}
+            </Button>
+          ))}
+        </ButtonGroup>
+      </FormGroup>
+      <Divider my="4x" />
+      <Dropdown
+        onChange={handleChange}
+        options={options}
+        renderOption={renderOption}
+        width={width}
+      >
+        {renderValue(value)}
+      </Dropdown>
+    </>
   );
 };
 
