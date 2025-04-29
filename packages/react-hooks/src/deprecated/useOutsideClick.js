2a735fddbc99d274e2e53b2134fd466c588b5768
Cheton Wu <cheton_wu@trendmicro.com>
2025-04-29 14:28:35 +0800

    feat: update the deprecation message for the `useOutsideClick` hook

--- packages/react-hooks/src/deprecated/useOutsideClick.js
+++ packages/react-hooks/src/deprecated/useOutsideClick.js
@@ -9,7 +9,13 @@ const useOutsideClick = (
   events = defaultEvents,
 ) => {
   useOnce(() => {
-    console.error('Warning: The `useOutsideClick` Hook has been renamed to `useClickOutside``.\n\nSee https://trendmicro-frontend.github.io/tonic-ui/react/latest/hooks for more information.');
+    if (process.env.NODE_ENV !== 'production') {
+      console.error(
+        'Warning: The `useOutsideClick` hook has been renamed to `useClickOutside`.\n\n' +
+        'This alias is planned to be removed in the next major release.\n' +
+        'See https://trendmicro-frontend.github.io/tonic-ui/react/latest/hooks for more information.'
+      );
+    }
   });
 
   useClickOutside(ref, handler, { events });
