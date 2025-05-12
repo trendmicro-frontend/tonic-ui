90f93703aa72e90f8f011febc1b5353104518d26
Cheton Wu <cheton_wu@trendmicro.com>
2025-05-12 17:50:17 +0800

    docs(testing-library): add `test-utils` for rendering components

---
+++ packages/react-docs/test-utils/render.js
@@ -0,0 +1,50 @@
+import { matchers } from '@emotion/jest';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import * as React from 'react';
+import {
+  PortalManager,
+  ToastManager,
+  TonicProvider,
+  createTheme,
+} from '@tonic-ui/react/src';
+
+// https://emotion.sh/docs/@emotion/jest#tohavestylerule
+//
+// Add the custom matchers provided by '@emotion/jest'
+expect.extend(matchers);
+
+const customRender = (ui, options) => {
+  const {
+    theme: themeOptions = {
+      cssVariables: {
+        prefix: 'tonic',
+        rootSelector: ':root',
+      },
+    },
+    ...rest
+  } = { ...options };
+
+  const wrapper = ({ children }) => (
+    <TonicProvider
+      colorMode={{
+        defaultValue: 'dark',
+      }}
+      theme={createTheme(themeOptions)}
+    >
+      <PortalManager>
+        <ToastManager>
+          {children}
+        </ToastManager>
+      </PortalManager>
+    </TonicProvider>
+  );
+
+  return render(ui, { wrapper, ...rest });
+};
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { customRender as render };
