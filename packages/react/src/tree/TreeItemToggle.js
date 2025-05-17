1882893fcb6657f2ec560a9fc068c7b5b96ceae9
cheton <cheton@gmail.com>
2025-05-18 00:14:15 +0800

    feat: integrate `useInteractiveActionHandlers` into `TreeItemToggle`

--- packages/react/src/tree/TreeItemToggle.js
+++ packages/react/src/tree/TreeItemToggle.js
@@ -1,9 +1,9 @@
-import { useEventCallback } from '@tonic-ui/react-hooks';
 import { ariaAttr, callEventHandlers } from '@tonic-ui/utils';
-import React, { forwardRef } from 'react';
 import { ensureFunction } from 'ensure-type';
+import React, { forwardRef, useCallback } from 'react';
 import { ButtonBase } from '../button';
 import { useDefaultProps } from '../default-props';
+import useInteractiveActionHandlers from '../utils/useInteractiveActionHandlers';
 import {
   useTreeItemToggleStyle,
 } from './styles';
@@ -15,49 +15,31 @@ const TreeItemToggle = forwardRef((inProps, ref) => {
     disabled,
     onClick: onClickProp,
     onKeyDown: onKeyDownProp,
+    onKeyUp: onKeyUpProp,
     ...rest
   } = useDefaultProps({ props: inProps, name: 'TreeItemToggle' });
   const {
     isExpanded,
     toggleExpansion,
   } = useTreeItem();
-
+  const preventNodeSelection = useCallback((event) => {
+    // Stop event bubbling to prevent the node from being selected
+    event.stopPropagation();
+  }, []);
   const styleProps = useTreeItemToggleStyle();
 
-  const onClick = useEventCallback((event) => {
-    // Stop event bubbling to prevent the node from being selected
-    event.stopPropagation();
-
-    if (disabled) {
-      event.preventDefault();
-      return;
-    }
-
-    ensureFunction(toggleExpansion)();
-  }, [disabled, toggleExpansion]);
-
-  const onKeyDown = useEventCallback((event) => {
-    // Stop event bubbling to prevent the node from being selected
-    event.stopPropagation();
-
-    if (disabled) {
-      event.preventDefault();
-      return;
-    }
-
-    if (event.key === 'Enter' || event.key === ' ') {
-      event.preventDefault(); // Prevent default scrolling for Space
-
-      ensureFunction(toggleExpansion)();
-    }
-  }, [disabled, toggleExpansion]);
+  const { onClick, onKeyDown, onKeyUp } = useInteractiveActionHandlers({
+    disabled,
+    onAction: () => ensureFunction(toggleExpansion)(),
+  });
 
   const getTreeItemToggleProps = () => ({
     'aria-disabled': ariaAttr(disabled),
     'aria-expanded': ariaAttr(isExpanded),
     disabled,
-    onClick: callEventHandlers(onClickProp, onClick),
-    onKeyDown: callEventHandlers(onKeyDownProp, onKeyDown),
+    onClick: callEventHandlers(onClickProp, preventNodeSelection, onClick),
+    onKeyDown: callEventHandlers(onKeyDownProp, preventNodeSelection, onKeyDown),
+    onKeyUp: callEventHandlers(onKeyUpProp, preventNodeSelection, onKeyUp),
     ref,
     role: 'button',
     tabIndex: 0,
