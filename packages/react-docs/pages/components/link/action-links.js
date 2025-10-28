469d574607d93bf884af7ce71a015239573768ec
cheton_wu <cheton_wu@trendmicro.com>
2025-10-28 10:08:50 +0800

    fix(eslint): remove unused imports

--- packages/react-docs/pages/components/link/action-links.js
+++ packages/react-docs/pages/components/link/action-links.js
@@ -1,11 +1,10 @@
-import { Link, Space, Stack } from '@tonic-ui/react';
+import { Link } from '@tonic-ui/react';
 import { EditIcon } from '@tonic-ui/react-icons';
 import React from 'react';
 
 const App = () => (
   <Link href="#">
-    <EditIcon />
-    <Space width="2x" />
+    <EditIcon ml="2x" />
     Edit
   </Link>
 );
