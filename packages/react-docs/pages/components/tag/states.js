a01fdfab484669d5b1f50259363012e95924283d
Cheton Wu <447801+cheton@users.noreply.github.com>
2026-01-12 23:12:24 +0800

    fix(react/tag): correct disabled close icon color on hover state (#1087)
    * fix(react/tag): correct disabled close icon color on hover state
    
    * Update changeset for react/tag hover state fix
    
    * test: update test snapshots

--- packages/react-docs/pages/components/tag/states.js
+++ packages/react-docs/pages/components/tag/states.js
@@ -1,4 +1,4 @@
-import { Stack, Tag } from '@tonic-ui/react';
+import { Divider, Stack, Tag } from '@tonic-ui/react';
 import React from 'react';
 
 const App = () => (
@@ -12,6 +12,17 @@ const App = () => (
       <Tag variant="outline">Normal</Tag>
       <Tag variant="outline" disabled>Disabled</Tag>
       <Tag variant="outline" error>Error</Tag>
+    </Stack>
+    <Divider />
+    <Stack direction="row" spacing="4x" shouldWrapChildren>
+      <Tag isClosable>Normal</Tag>
+      <Tag disabled isClosable>Disabled</Tag>
+      <Tag error isClosable>Error</Tag>
+    </Stack>
+    <Stack direction="row" spacing="4x" shouldWrapChildren>
+      <Tag variant="outline" isClosable>Normal</Tag>
+      <Tag variant="outline" disabled isClosable>Disabled</Tag>
+      <Tag variant="outline" error isClosable>Error</Tag>
     </Stack>
   </Stack>
 );
