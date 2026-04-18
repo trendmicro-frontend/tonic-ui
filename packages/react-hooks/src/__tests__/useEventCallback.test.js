59d35328536d0f75f31eefa340ae6cfa583d2afd
cheton <cheton@gmail.com>
2026-04-18 22:35:07 +0800

    chore(react-hooks): remove unused React imports and fix lint errors

--- packages/react-hooks/src/__tests__/useEventCallback.test.js
+++ packages/react-hooks/src/__tests__/useEventCallback.test.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useConst, useEventCallback } from '@tonic-ui/react-hooks/src';
 
 describe('useEventCallback', () => {
