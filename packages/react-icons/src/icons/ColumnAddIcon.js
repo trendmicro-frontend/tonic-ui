84fc3490b0bb63b9be7e6bbe3e81a850b44a729a
cheton <cheton@gmail.com>
2026-09-14 22:46:01 +0800

    feat(react-icons): add column-add and column-delete icons

---
+++ packages/react-icons/src/icons/ColumnAddIcon.js
@@ -0,0 +1,7 @@
+// AUTO-GENERATED FILE. DO NOT EDIT.
+import createSVGIcon from '../utils/createSVGIcon';
+
+export default createSVGIcon(
+  <><path d="M12 9H16V11H12V15H10V11H6V9H10V5H12V9Z" /><path d="M16 7H14V2H2V9H4V11H0V0H16V7Z" /></>,
+  'ColumnAddIcon',
+);
